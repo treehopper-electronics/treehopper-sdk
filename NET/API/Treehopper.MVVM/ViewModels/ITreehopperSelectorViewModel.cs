56936a88bd64895d6195d588fe7a153259d822d2
Jay Carlson <jcarlson@unl.edu>
2016-07-12 02:04:26 -0500

    Moved DesignTimeViewModel

---
+++ NET/API/Treehopper.MVVM/ViewModels/ITreehopperSelectorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
+
+namespace Treehopper.Mvvm.ViewModel
+{
+    public interface ITreehopperSelectorViewModel
+    {
+        ObservableCollection<TreehopperUsb> Boards { get; }
+        bool CanChangeBoardSelection { get; set; }
+        TreehopperUsb SelectedBoard { get; set; }
+        RelayCommand WindowClosing { get; set; }
+        RelayCommand ConnectCommand { get; set; }
+        string ConnectButtonText { get; set; }
+        RelayCommand CloseCommand { get; set; }
+    }
+}
