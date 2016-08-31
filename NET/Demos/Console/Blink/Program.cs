c90a246e6b0bab1d4190cd3ab11601f36b6f633b
Jay Carlson <jcarlson@unl.edu>
2016-08-30 19:14:36 -0500

    Update Win32 Console Blink

--- NET/Demos/Console/Blink/Program.cs
+++ NET/Demos/Console/Blink/Program.cs
@@ -8,15 +8,9 @@ using System.Collections.Generic;
 namespace Blink
 {
     /// <summary>
-    /// This demo blinks an LED attached to pin 1, using basic procedural programming.
+    /// This demo blinks the built-in LED using basic procedural programming.
     /// </summary>
     /// <remarks>
-    /// <para>
-    /// Treehopper provides an asynchronous, event-driven API that helps your apps respond to plug / unplug detection.
-    /// While this is the recommended way of working with TreehopperUsb references, many users new to C# and high-level languages
-    /// in general may be uncomfortable with event-driven programming. To learn more about asynchronous usage, check out the
-    /// BlinkEventDriven example.
-    /// </para>
     /// <para>
     /// This example illustrates how to work with Treehopper boards procedurally to blink an LED. 
     /// </para>
@@ -25,14 +19,15 @@ namespace Blink
     {
         static void Main(string[] args)
         {
-            RunBlink();
+            RunBlink().Wait();
         }
 
-        static async void RunBlink()
+        static async Task RunBlink()
         {
             while(true)
             {
                 Console.Write("Waiting for board...");
+
                 // Get a reference to the first TreehopperUsb board connected. This will await indefinitely until a board is connected.
                 TreehopperUsb Board = await ConnectionService.Instance.First();        
                 Console.WriteLine("Found board: " + Board);
@@ -40,15 +35,14 @@ namespace Blink
                 // You must explicitly open a board before communicating with it
                 await Board.Connect();
 
-                Board.Pins[0].Mode = PinMode.PushPullOutput;
-
-                while(true)
+                while(Board.IsConnected)
                 {
-                    //Board.Led = !Board.Led;
-                    Board.Pins[0].DigitalValue = !Board.Pins[0].DigitalValue;
-                    await Task.Delay(1000);
-                }
+                    // toggle the LED
+                    Board.Led = !Board.Led; 
 
+                    // wait 500 ms
+                    await Task.Delay(500);
+                }
 
                 // We arrive here when the board has been disconnected
                 Console.WriteLine("Board has been disconnected.");
