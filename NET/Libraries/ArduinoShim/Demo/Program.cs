24621222265bc0ea483a57a92623e4264b375fce
Jay Carlson <jcarlson@unl.edu>
2016-09-03 21:01:38 -0500

    Add ArduinoShim library

---
+++ NET/Libraries/ArduinoShim/Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treehopper;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App().Wait();
+        }
+
+        static async Task App()
+        {
+            var board = await ConnectionService.Instance.First();
+            var blinkSketch = new Blink(board);
+            blinkSketch.Serial.WriteRequested += (msg) => Console.Write(msg); // if we want Serial.write() to print to our screen, we do this
+            blinkSketch.Run();
+        }
+    }
+}
---
+++ NET/Libraries/ArduinoShim/Library/SerialShim.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treehopper;
+
+namespace ArduinoShim
+{
+    public delegate void SerialShimWriter(string message);
+
+    public class SerialShim
+    {
+        TreehopperUsb board;
+        bool redirectToDebug = true;
+
+        public event SerialShimWriter WriteRequested;
+
+        public SerialShim(TreehopperUsb board)
+        {
+            this.board = board;
+        }
+
+        public void begin(int baud, bool redirectToDebugStream = true)
+        {
+            this.redirectToDebug = redirectToDebugStream;
+            if (redirectToDebugStream)
+                return;
+
+            board.Uart.Baud = baud;
+            board.Uart.Enabled = true;
+        }
+
+        public void write(dynamic value)
+        {
+            if(redirectToDebug)
+            {
+                string str = value.ToString();
+                Debug.WriteLine(str);
+                if (WriteRequested != null)
+                    WriteRequested(str);
+            } else
+            {
+                board.Uart.Send(value);
+            }
+        }
+    }
+}
