caf0e25757ad719da2e8a0b0b6ba3c72565861f8
Jay Carlson <jcarlson@unl.edu>
2016-09-03 21:01:38 -0500

    Add ArduinoShim library

---
+++ NET/Libraries/ArduinoShim/Demo/Blink.cs
@@ -0,0 +1,33 @@
+﻿using ArduinoShim;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treehopper;
+namespace Demo
+{
+    class Blink : Sketch
+    {
+        public Blink(TreehopperUsb board, bool throwExceptions = true) : base(board, throwExceptions)
+        {
+        }
+
+        public override void setup()
+        {
+            Serial.begin(9600);
+            Serial.write("Starting sketch...\n");
+            pinMode(ledPin, OUTPUT);      // sets the digital pin as output          
+        }
+
+        public override void loop()
+        {
+            digitalWrite(ledPin, HIGH);   // sets the LED on
+            delay(1000);                  // waits for a second
+            digitalWrite(ledPin, LOW);    // sets the LED off
+            delay(1000);                  // waits for a second
+        }
+
+
+    }
+}
