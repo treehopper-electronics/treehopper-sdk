fdf25ba3f249be75fac38c8339055be5aee53169
Jay Carlson <jcarlson@unl.edu>
2016-09-27 22:15:17 -0500

    Move useful RandomString function into Utilities

--- NET/API/Treehopper/Utilities.cs
+++ NET/API/Treehopper/Utilities.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Treehopper
 {
@@ -76,6 +77,14 @@ namespace Treehopper
                     retVal += ".";
             }
             return retVal;
+        }
+
+        static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
