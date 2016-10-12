fe1c96c4078bb364dbd4df589c9f6c4b5f61a03b
Jay Carlson <jcarlson@unl.edu>
2016-10-11 20:18:17 -0500

    codegen stuff

--- Firmware-EFM8/src/Interrupts.c
+++ Firmware-EFM8/src/Interrupts.c
@@ -134,3 +134,17 @@ SI_INTERRUPT (TIMER4_ISR, TIMER4_IRQn) {
 //	SoftPwm_Task();
 }
 
+//-----------------------------------------------------------------------------
+// UART0_ISR
+//-----------------------------------------------------------------------------
+//
+// UART0 ISR Content goes here. Remember to clear flag bits:
+// SCON0::RI (Receive Interrupt Flag)
+// SCON0::TI (Transmit Interrupt Flag)
+//
+//-----------------------------------------------------------------------------
+//SI_INTERRUPT (UART0_ISR, UART0_IRQn)
+//{
+//
+//}
+
