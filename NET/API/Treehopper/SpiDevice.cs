b1940f8cb18689e6dcfa68703053cfa305532c6d
Jay Carlson <jcarlson@unl.edu>
2016-11-08 00:36:09 -0600

    Base class for SPI devices to inherit from

---
+++ NET/API/Treehopper/SpiDevice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treehopper
+{
+    public class SpiDevice
+    {
+        private Pin cs;
+        private Spi spi;
+        private double speed;
+        public SpiDevice(Spi SpiModule, Pin ChipSelect, double SpeedMHz = 1)
+        {
+            this.cs = ChipSelect;
+            this.spi = SpiModule;
+            this.speed = SpeedMHz;
+            spi.Enabled = true;
+        }
+
+        protected async Task<byte[]> SendReceive(byte[] dataToSend, BurstMode burst = BurstMode.NoBurst)
+        {
+            spi.ChipSelect = cs;
+            spi.Frequency = speed;
+            return await spi.SendReceive(dataToSend, burst);
+        }
+    }
+}
