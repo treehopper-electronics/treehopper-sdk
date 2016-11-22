89a14109e2e9155c789dcbceebb4fbf6d9de10db
Jay Carlson <jcarlson@unl.edu>
2016-11-21 23:28:35 -0600

    Digital input pin init

--- Firmware-EFM8/inc/InitDevice.h
+++ Firmware-EFM8/inc/InitDevice.h
@@ -17,8 +17,7 @@ extern void enter_DefaultMode_from_RESET(void);
 
 // $[Config(Per-Module Mode)Transition Prototypes]
 extern void WDT_0_enter_DefaultMode_from_RESET(void);
-extern void PORTS_0_enter_DefaultMode_from_RESET(void);
-extern void PORTS_1_enter_DefaultMode_from_RESET(void);
+extern void PORTS_3_enter_DefaultMode_from_RESET(void);
 extern void PBCFG_0_enter_DefaultMode_from_RESET(void);
 extern void ADC_0_enter_DefaultMode_from_RESET(void);
 extern void VREF_0_enter_DefaultMode_from_RESET(void);
