dc406f4679e801db37bb8e6255b25764143337a0
Jay Carlson <jcarlson@unl.edu>
2016-11-27 16:19:11 -0600

    Don't include docs for code that isn't ours!

---
+++ NET/Sandbox/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treehopper;
+using Treehopper.Libraries;
+namespace Sandbox
+{
+    /// <summary>
+    /// This is the in-tree "sandbox" app that developers use to quickly test stuff out
+    /// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            App().Wait();
+        }
+
+        static async Task App()
+        {
+            var board = await ConnectionService.Instance.GetFirstDeviceAsync();
+            await board.ConnectAsync();
+
+            board.Pins[0].Mode = PinMode.PushPullOutput;
+
+            while(board.IsConnected && !Console.KeyAvailable)
+            {
+                // do stuff
+                board.Pins[0].ToggleOutput();
+            }
+        }
+    }
+}
