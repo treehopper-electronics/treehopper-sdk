8143d04d63f7f75b07e5ab63996a4ea8f2e41b9d
Jay Carlson <jcarlson@unl.edu>
2016-11-26 20:50:39 -0600

    Refactoring Pin interfaces

---
+++ NET/API/Treehopper/IDigitalPin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treehopper
+{
+    public interface IDigitalPinBase
+    {
+        bool DigitalValue { get; set; }
+    }
+    public interface IDigitalOutPin : IDigitalPinBase
+    {
+        void ToggleOutput();
+
+        void MakeDigitalPushPullOut();
+    }
+
+    public interface IDigitalInPin : IDigitalPinBase
+    {
+        Task<bool> AwaitDigitalValueChange();
+
+        event OnDigitalInValueChanged DigitalValueChanged;
+        void MakeDigitalIn();
+    }
+
+    public interface IDigitalIOPin : IDigitalInPin, IDigitalOutPin
+    {
+
+    }
+}
