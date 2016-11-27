737554b8e9f42dcc5e17ad119cd5b02b0c60772b
Jay Carlson <jcarlson@unl.edu>
2016-11-27 14:49:31 -0600

    Add more chip-select functions

--- NET/API/Treehopper/SpiDevice.cs
+++ NET/API/Treehopper/SpiDevice.cs
@@ -10,17 +10,24 @@ namespace Treehopper
     {
         private Pin cs;
         private Spi spi;
+        private SPIMode mode;
         private double speed;
-        public SpiDevice(Spi SpiModule, Pin ChipSelect, double SpeedMHz = 1)
+        private ChipSelectMode csMode;
+
+        public SpiDevice(Spi SpiModule, Pin ChipSelect, double SpeedMHz = 1, SPIMode mode = SPIMode.Mode00, ChipSelectMode csMode = ChipSelectMode.SpiActiveLow)
         {
+            this.csMode = csMode;
             this.cs = ChipSelect;
             this.spi = SpiModule;
             this.speed = SpeedMHz;
+            this.mode = mode;
+
             spi.Enabled = true;
         }
 
         public async Task<byte[]> SendReceive(byte[] dataToSend, BurstMode burst = BurstMode.NoBurst)
         {
+            spi.ChipSelectMode = csMode;
             spi.ChipSelect = cs;
             spi.Frequency = speed;
             return await spi.SendReceive(dataToSend, burst);
