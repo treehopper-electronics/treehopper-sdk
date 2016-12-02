3877beaf33458bc47a3a820ee0f279477f608ae4
Jay Carlson <jcarlson@unl.edu>
2016-12-02 16:14:50 -0600

    Use Pins, not ints. We ain't Arduino!

--- NET/Demos/Console/SoftPwm/Program.cs
+++ NET/Demos/Console/SoftPwm/Program.cs
@@ -16,25 +16,26 @@ namespace Treehopper.Demos.SoftPwm
         static TreehopperUsb board;
         static async void Run()
         {
-            int pinNumber = 10;
             Console.Write("Looking for board...");
             board = await ConnectionService.Instance.GetFirstDeviceAsync();
             Console.WriteLine("Board found.");
-            Console.WriteLine(String.Format("Connecting to {0} and starting SoftPwm on Pin{1}", board, pinNumber));
             await board.ConnectAsync();
-            board[pinNumber].SoftPwm.Enabled = true;
+            var pin = board[1];
+
+            pin.SoftPwm.Enabled = true;
+            pin.SoftPwm.DutyCycle = 0.8;
             int step = 10;
             int rate = 25;
             while (true)
             {
                 for (int i = 0; i < 256; i = i + step)
                 {
-                    board[pinNumber].SoftPwm.DutyCycle = i / 255.0;
+                    pin.SoftPwm.DutyCycle = i / 255.0;
                     await Task.Delay(rate);
                 }
                 for (int i = 255; i > 0; i = i - step)
                 {
-                    board[pinNumber].SoftPwm.DutyCycle = i / 255.0;
+                    pin.SoftPwm.DutyCycle = i / 255.0;
                     await Task.Delay(rate);
                 }
             }
