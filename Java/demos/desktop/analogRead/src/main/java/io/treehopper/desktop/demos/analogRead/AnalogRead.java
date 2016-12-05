e8af3b89c554996da6046e5cc11d02a2a370e68d
Jay Carlson <jcarlson@unl.edu>
2016-12-04 22:51:54 -0600

    Updated project source tree; added analogRead demo

---
+++ Java/demos/desktop/analogRead/src/main/java/io/treehopper/desktop/demos/analogRead/AnalogRead.java
@@ -0,0 +1,27 @@
+package io.treehopper.desktop.demos.analogRead;
+
+import java.util.ArrayList;
+import io.treehopper.desktop.*;
+import io.treehopper.*;
+
+public class AnalogRead {
+    public static void main(String[] args) {
+        ConnectionService service = new ConnectionService();
+        ArrayList<TreehopperUsb> boards = service.getBoards();
+        TreehopperUsb hopper = boards.get(0);
+        hopper.connect();
+
+        Pin adcPin = hopper.Pins[10];
+        adcPin.setMode(PinMode.AnalogInput);
+
+        while (true) {
+            double value = adcPin.getAdcValue();
+            System.out.println(value);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
