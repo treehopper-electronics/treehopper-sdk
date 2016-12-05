d2616ffcbff3ac4529b3b815eb3a4132f0c4f148
Jay Carlson <jcarlson@unl.edu>
2016-12-04 19:59:03 -0600

    clean-up

--- NET/API/Treehopper/Treehopper.cs
+++ NET/API/Treehopper/Treehopper.cs
@@ -71,83 +71,14 @@ namespace Treehopper
     /// 
     /// </remarks>
     public class TreehopperUsb : INotifyPropertyChanged, IDisposable, IComparable, IEquatable<TreehopperUsb>, IEqualityComparer<TreehopperUsb>
-	{
-        const int MinimumSupportedFirmwareVersion = 110;
-
-        // Settings
+    {
         public static Settings Settings { get; set; } = new Settings();
 
 
-        public Pin this[int index]
-        {
-            get
-            {
-                return Pins[index];
-            }
-        }
-
-        #region Pin Definitions
-
-
-
-        #endregion
-
-        #region Modules
-
-        internal readonly AsyncLock ComsLock = new AsyncLock();
-
-        /// <summary>
-        /// I2C module
-        /// </summary>
-        public I2c I2c { get; private set; }
-
-		/// <summary>
-		/// SPI module
-		/// </summary>
-		public Spi Spi { get; private set; }
-
-        public Uart Uart { get; private set; }
-
-        public Pwm Pwm1 { get; private set; }
-        public Pwm Pwm2 { get; private set; }
-        public Pwm Pwm3 { get; private set; }
-        public ParallelInterface ParallelInterface { get; private set; }
-
-        /// <summary>
-        /// Instance of SoftPwmMgr
-        /// </summary>
-        internal SoftPwmManager SoftPwmMgr { get; private set; }
-
-        public PwmManager PwmManager { get; private set; }
-
-        #endregion
-
+        const int MinimumSupportedFirmwareVersion = 110;
+        byte[] pinStateBuffer;
+        private IConnection connection;
         private bool led = false;
-            public bool Led
-        {
-            get
-            {
-                return led;
-            }
-            set
-            {
-                if (value == led)
-                    return;
-                led = value;
-
-                byte[] DataToSend = new byte[2];
-                DataToSend[0] = (byte)DeviceCommands.LedConfig;
-                DataToSend[1] = (byte)(led ? 0x01 : 0x00); // Unicode 16-bit strings are 2 bytes per character
-                sendPeripheralConfigPacket(DataToSend);
-            }
-        }
-
-		byte[] pinStateBuffer;
-
-		private IConnection connection;
-
-
-        public int NumberOfPins { get { return 20; } }
 
         public TreehopperUsb(IConnection treehopperUsbConnection)
         {
@@ -172,72 +103,69 @@ namespace Treehopper
             ParallelInterface = new ParallelInterface(this);
         }
 
+        #region Modules
+
+        internal readonly AsyncLock ComsLock = new AsyncLock();
+
+        /// <summary>
+        /// I2C module
+        /// </summary>
+        public I2c I2c { get; private set; }
+
+        /// <summary>
+        /// SPI module
+        /// </summary>
+        public Spi Spi { get; private set; }
+
+        public Uart Uart { get; private set; }
+
+        public Pwm Pwm1 { get; private set; }
+        public Pwm Pwm2 { get; private set; }
+        public Pwm Pwm3 { get; private set; }
+        public ParallelInterface ParallelInterface { get; private set; }
+
+        /// <summary>
+        /// Instance of SoftPwmMgr
+        /// </summary>
+        internal SoftPwmManager SoftPwmMgr { get; private set; }
+
+        public PwmManager PwmManager { get; private set; }
+
+
+        public bool Led
+        {
+            get
+            {
+                return led;
+            }
+            set
+            {
+                if (value == led)
+                    return;
+                led = value;
+
+                byte[] DataToSend = new byte[2];
+                DataToSend[0] = (byte)DeviceCommands.LedConfig;
+                DataToSend[1] = (byte)(led ? 0x01 : 0x00); // Unicode 16-bit strings are 2 bytes per character
+                sendPeripheralConfigPacket(DataToSend);
+            }
+        }
+
+        #endregion
+
+        public Pin this[int index]
+        {
+            get
+            {
+                return Pins[index];
+            }
+        }
+        public bool IsConnected { get; internal set; }
+
+        public int NumberOfPins { get { return 20; } }
+
         public IConnection Connection { get { return connection; } }
         
-        /// <summary>
-        /// Reboots the board.
-        /// </summary>
-        /// <remarks>
-        /// Calling this method will automatically call the Disconnect() method, and no further communication will be possible until the board is reopened.
-        /// </remarks>
-        public void Reboot()
-		{
-			sendPeripheralConfigPacket(new byte[] { (byte)DeviceCommands.Reboot });
-			Disconnect(); // This is called by the manager when the board is removed, but call it here just in case the manager isn't running.
-		}
-		public void RebootIntoBootloader()
-		{
-			sendPeripheralConfigPacket(new byte[] { (byte)DeviceCommands.EnterBootloader });
-			Disconnect(); // This is called by the manager when the board is removed, but call it here just in case the manager isn't running.
-		}
-
-		/// <summary>
-		/// Update the serial number on the device.
-		/// </summary>
-		/// <param name="serialNumber">A 60-character (or fewer) string containing the new serial number</param>
-		/// <remarks>
-		/// While the name is immediately written to the device and the Name property is updated immediately, the changes 
-		/// will not take effect to other applications until the device is reset. This can be done by calling <see cref="Reset"/>
-		/// </remarks>
-		public Task UpdateSerialNumber(string serialNumber)
-		{
-			if (serialNumber.Length > 60)
-				throw new Exception("String must be 15 characters or less");
-
-			byte[] bytes = Encoding.UTF8.GetBytes(serialNumber);
-			byte[] DataToSend = new byte[bytes.Length + 2];
-			DataToSend[0] = (byte)DeviceCommands.FirmwareUpdateSerial;
-			DataToSend[1] = (byte)(serialNumber.Length); // Unicode 16-bit strings are 2 bytes per character
-			bytes.CopyTo(DataToSend, 2);
-			sendPeripheralConfigPacket(DataToSend);
-            return Task.Delay(100); // wait a bit for the flash operation to finish (global interrupts are disabled during programming)
-        }
-
-		/// <summary>
-		/// Update the name of the device.
-		/// </summary>
-		/// <param name="deviceName">A 60-character (or fewer) string containing the new name of the device.</param>
-		/// <remarks>
-		/// While the name is immediately written to the device and the Name property is updated immediately, the changes 
-		/// will not take effect to other applications until the device is reset. This can be done by calling <see cref="Reset"/>
-		/// </remarks>
-		public Task UpdateDeviceName(string deviceName)
-		{
-			if (deviceName.Length > 60)
-				throw new Exception("Device name must be 60 characters or less");
-			byte[] DataToSend = new byte[deviceName.Length + 2];
-			DataToSend[0] = (byte)DeviceCommands.FirmwareUpdateName;
-			DataToSend[1] = (byte)(deviceName.Length); // Unicode 16-bit strings are 2 bytes per character
-			byte[] stringData = Encoding.UTF8.GetBytes(deviceName);
-			stringData.CopyTo(DataToSend, 2);
-			sendPeripheralConfigPacket(DataToSend);
-            return Task.Delay(100); // wait a bit for the flash operation to finish (global interrupts are disabled during programming)
-        }
-
-		~TreehopperUsb()
-		{
-			Dispose();
-		}
 
 		/// <summary>
 		/// The unique identification string for the board.
@@ -253,7 +181,7 @@ namespace Treehopper
 		/// attempt to convert this to a numeric type.
 		/// </para>
 		/// <para>
-		/// This property is used internally by <see cref="TreehopperManager"/> to match unique devices. Attaching two devices with the same
+		/// This property is used internally by <see cref="IConnectionService"/> to match unique devices. Attaching two devices with the same
 		/// serial number will produce undesirable application behavior.
 		/// </para>
 		/// <para>
@@ -316,10 +244,76 @@ namespace Treehopper
         /// </summary>
         public ObservableCollection<Pin> Pins { get; set; } = new ObservableCollection<Pin>();
 
-		/// <summary>
-		/// Open the TreehopperBoard. The board must be opened before any other methods are called.
-		/// </summary>
-		public async Task<bool> ConnectAsync()
+
+        /// <summary>
+        /// Reboots the board.
+        /// </summary>
+        /// <remarks>
+        /// Calling this method will automatically call the Disconnect() method, and no further communication will be possible until the board is reopened.
+        /// </remarks>
+        public void Reboot()
+        {
+            sendPeripheralConfigPacket(new byte[] { (byte)DeviceCommands.Reboot });
+            Disconnect(); // This is called by the manager when the board is removed, but call it here just in case the manager isn't running.
+        }
+        public void RebootIntoBootloader()
+        {
+            sendPeripheralConfigPacket(new byte[] { (byte)DeviceCommands.EnterBootloader });
+            Disconnect(); // This is called by the manager when the board is removed, but call it here just in case the manager isn't running.
+        }
+
+        /// <summary>
+        /// Update the serial number on the device.
+        /// </summary>
+        /// <param name="serialNumber">A 60-character (or fewer) string containing the new serial number</param>
+        /// <remarks>
+        /// While the name is immediately written to the device and the Name property is updated immediately, the changes 
+        /// will not take effect to other applications until the device is reset. This can be done by calling <see cref="Reboot()"/>
+        /// </remarks>
+        public Task UpdateSerialNumber(string serialNumber)
+        {
+            if (serialNumber.Length > 60)
+                throw new Exception("String must be 15 characters or less");
+
+            byte[] bytes = Encoding.UTF8.GetBytes(serialNumber);
+            byte[] DataToSend = new byte[bytes.Length + 2];
+            DataToSend[0] = (byte)DeviceCommands.FirmwareUpdateSerial;
+            DataToSend[1] = (byte)(serialNumber.Length); // Unicode 16-bit strings are 2 bytes per character
+            bytes.CopyTo(DataToSend, 2);
+            sendPeripheralConfigPacket(DataToSend);
+            return Task.Delay(100); // wait a bit for the flash operation to finish (global interrupts are disabled during programming)
+        }
+
+        /// <summary>
+        /// Update the name of the device.
+        /// </summary>
+        /// <param name="deviceName">A 60-character (or fewer) string containing the new name of the device.</param>
+        /// <remarks>
+        /// While the name is immediately written to the device and the Name property is updated immediately, the changes 
+        /// will not take effect to other applications until the device is reset. This can be done by calling <see cref="Reboot()"/>
+        /// </remarks>
+        public Task UpdateDeviceName(string deviceName)
+        {
+            if (deviceName.Length > 60)
+                throw new Exception("Device name must be 60 characters or less");
+            byte[] DataToSend = new byte[deviceName.Length + 2];
+            DataToSend[0] = (byte)DeviceCommands.FirmwareUpdateName;
+            DataToSend[1] = (byte)(deviceName.Length); // Unicode 16-bit strings are 2 bytes per character
+            byte[] stringData = Encoding.UTF8.GetBytes(deviceName);
+            stringData.CopyTo(DataToSend, 2);
+            sendPeripheralConfigPacket(DataToSend);
+            return Task.Delay(100); // wait a bit for the flash operation to finish (global interrupts are disabled during programming)
+        }
+
+        ~TreehopperUsb()
+        {
+            Dispose();
+        }
+
+        /// <summary>
+        /// Open the TreehopperBoard. The board must be opened before any other methods are called.
+        /// </summary>
+        public async Task<bool> ConnectAsync()
 		{
             if (Version < MinimumSupportedFirmwareVersion)
                 Debug.WriteLine("NOTICE: The specified board has an old firmware version. Please use the Firmware Updater to load a firmware image with a minimum version of " + MinimumSupportedFirmwareVersion);
@@ -368,8 +362,6 @@ namespace Treehopper
 
             }
         }
-
-        public bool IsConnected { get; internal set; }
 
 		internal void sendPinConfigPacket(byte[] data)
 		{
@@ -480,13 +472,19 @@ namespace Treehopper
 				return false;
 			return (x.ToString() == y.ToString());
 		}
-		/// <summary>
-		/// Hash, obtained from the serial number of the device.
-		/// </summary>
-		/// <param name="board">the Treehopper board to calculate the hash for</param>
-		/// <returns>The hash code for the board</returns>
-		/// <see cref="Object.GetHashCode"/>
-		public int GetHashCode(TreehopperUsb board)
+
+        public override int GetHashCode()
+        {
+            return GetHashCode(this);
+        }
+
+        /// <summary>
+        /// Hash, obtained from the serial number of the device.
+        /// </summary>
+        /// <param name="board">the Treehopper board to calculate the hash for</param>
+        /// <returns>The hash code for the board</returns>
+        /// <see cref="Object.GetHashCode"/>
+        public int GetHashCode(TreehopperUsb board)
 		{
 			if (board != null)
 				if (board.Name != null)
@@ -561,4 +559,11 @@ namespace Treehopper
 				return 0;
 		}
 	}
+
+    public static class TaskExtensions
+    {
+        public static void Forget(this Task task)
+        {
+        }
+    }
 }
