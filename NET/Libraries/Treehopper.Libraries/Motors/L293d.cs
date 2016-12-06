2ad76f9b0f0f5e456a9611652239e54cda1abbdb
Jay Carlson <jcarlson@unl.edu>
2016-12-06 13:04:29 -0600

    Added L293D motor driver

---
+++ NET/Libraries/Treehopper.Libraries/Motors/L293d.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treehopper.Libraries.Motors
+{
+    /// <summary>
+    /// 1A, 36-V dual H-bridge motor controller
+    /// </summary>
+    public class L293d
+    {
+        /// <summary>
+        /// Channel A H-bridge
+        /// </summary>
+        public DualHalfBridge ChannelA { get; set; }
+
+        /// <summary>
+        /// Channel B H-Bridge
+        /// </summary>
+        public DualHalfBridge ChannelB { get; set; }
+    }
+}
