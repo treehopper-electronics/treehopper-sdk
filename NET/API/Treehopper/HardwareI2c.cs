9de4fb7078b2e0062be2e2916c3292ce705d78f5
Jay Carlson <jcarlson@unl.edu>
2016-12-05 20:15:09 -0600

    docs

--- NET/API/Treehopper/HardwareI2c.cs
+++ NET/API/Treehopper/HardwareI2c.cs
@@ -6,12 +6,34 @@ using System.Threading.Tasks;
 
 namespace Treehopper
 {
+    /// <summary>
+    /// Describes the transfer error, if not Success, that occured
+    /// </summary>
     public enum I2cTransferError
     {
+        /// <summary>
+        /// Bus arbitration was lost
+        /// </summary>
         ArbitrationLostError,
+
+        /// <summary>
+        /// The slave device failed to Nack back.
+        /// </summary>
         NackError,
+
+        /// <summary>
+        /// Unknown error
+        /// </summary>
         UnknownError,
+
+        /// <summary>
+        /// Tx buffer underrun error
+        /// </summary>
         TxunderError,
+
+        /// <summary>
+        /// Successful transaction
+        /// </summary>
         Success = 0x255
     }
     //public enum I2cMode { Master, Slave };
--- NET/API/Treehopper/I2c.cs
+++ NET/API/Treehopper/I2c.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace Treehopper
 {
+    /// <summary>
+    /// Describes an I2c master device.
+    /// </summary>
+    /// <remarks>
+    /// <para>To support the widest variety of hardware configurations, peripheral libraries should always use this class instead of <see cref="HardwareI2c"/>.</para>
+    /// </remarks>
     public interface I2c
     {
+        /// <summary>
+        /// The speed, in KHz, that the bus should be operated at. The default is 100 KHz for most modules.
+        /// </summary>
         double Speed { get; set; }
+
+        /// <summary>
+        /// Whether this I2c port should be enabled or disabled
+        /// </summary>
         bool Enabled { get; set; }
+
+        /// <summary>
+        /// Perform an I2c transaction to a peripheral slave device using this port.
+        /// </summary>
+        /// <param name="address">The 7-bit address of the slave. Valid range is 0-127.</param>
+        /// <param name="dataToWrite">A byte array of the data to write to the device.</param>
+        /// <param name="numBytesToRead">The number of bytes to read and return.</param>
+        /// <returns>The bytes read from the device.</returns>
         Task<byte[]> SendReceive(byte address, byte[] dataToWrite, byte numBytesToRead);
     }
 }
