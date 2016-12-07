fca428551e709672687d9f6873cb2d806a47b9be
Jay Carlson <jcarlson@unl.edu>
2016-12-07 03:30:32 -0600

    Added temperature sensor

---
+++ Java/treehopper.libraries/src/main/java/io/treehopper/libraries/sensors/temperature/Temperature.java
@@ -0,0 +1,12 @@
+package io.treehopper.libraries.sensors.temperature;
+
+/**
+ * Created by jay on 12/7/2016.
+ */
+public interface Temperature {
+    double getTemperatureCelsius();
+
+    double getTemperatureFahrenheit();
+
+    double getTemperatureKelvin();
+}
---
+++ Java/treehopper.libraries/src/main/java/io/treehopper/libraries/sensors/temperature/TemperatureSensor.java
@@ -0,0 +1,18 @@
+package io.treehopper.libraries.sensors.temperature;
+
+/**
+ * Created by jay on 12/7/2016.
+ */
+
+public abstract class TemperatureSensor implements Temperature {
+
+    @Override
+    public double getTemperatureFahrenheit() {
+        return getTemperatureCelsius() * 9.0 / 5.0 + 32.0;
+    }
+
+    @Override
+    public double getTemperatureKelvin() {
+        return getTemperatureCelsius() + 273.15;
+    }
+}
