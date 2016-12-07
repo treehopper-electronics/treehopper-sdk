5bfee62828fb231de8713deaeb2d596148d20287
Jay Carlson <jcarlson@unl.edu>
2016-12-07 03:27:22 -0600

    Added MLX90615 temperature sensor

---
+++ NET/Libraries/Demos/Sensors/Mlx90615/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treehopper;
+using Treehopper.Libraries.Sensors.Temperature;
+namespace Mlx90615Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App().Wait();
+        }
+
+        static async Task App()
+        {
+            var board = await ConnectionService.Instance.GetFirstDeviceAsync();
+            await board.ConnectAsync();
+
+            var tempSensor = new Mlx90615(board.I2c);
+
+            while(true)
+            {
+                Console.Write("Ambient temperature: ");
+                Console.WriteLine(tempSensor.Ambient.TemperatureFahrenheit);
+
+                Console.Write("Object temperature: ");
+                Console.WriteLine(tempSensor.Object.TemperatureFahrenheit);
+
+                Console.Write("Raw IR data: ");
+                Console.WriteLine(tempSensor.RawIrData);
+
+                Console.WriteLine();
+
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
---
+++ NET/Libraries/Treehopper.Libraries/Sensors/Temperature/Mlx90615.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treehopper.Libraries.Sensors.Temperature
+{
+    public class Mlx90615
+    {
+        private SMBusDevice dev;
+
+        public Mlx90615(I2c module)
+        {
+            this.dev = new SMBusDevice(0x5B, module, 30);
+            Object =  new TempRegister(dev, 0x27);
+            Ambient = new TempRegister(dev, 0x26);
+        }
+        public Temperature Ambient { get; private set; }
+        public Temperature Object { get; private set; }
+
+        public int RawIrData { get { return dev.ReadWordData(0x25).Result;  } }
+
+        internal class TempRegister : TemperatureSensor
+        {
+            private SMBusDevice dev;
+            private byte register;
+
+            public TempRegister(SMBusDevice dev, byte register)
+            {
+                this.register = register;
+                this.dev = dev;
+            }
+
+            public override double TemperatureCelsius
+            {
+                get
+                {
+                    var data = dev.ReadWordData(register).Result;
+
+                    data &= 0x7FFF; // chop off the error bit of the high byte
+                    return (data * 0.02 - 273.15);
+                }
+            }
+        }
+    }
+}
