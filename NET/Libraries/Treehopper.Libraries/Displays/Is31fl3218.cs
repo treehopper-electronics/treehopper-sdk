53711cefc427346c45808840a7148d68af0417a0
Jay Carlson <jcarlson@unl.edu>
2016-12-19 23:09:09 -0600

    Added IS31FL3218

---
+++ NET/Libraries/Treehopper.Libraries/Displays/Is31fl3218.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treehopper.Libraries.Displays
+{
+    /// <summary>
+    /// IS31FL3218 18-channel LED constant-current PWM driver
+    /// </summary>
+    public class Is31fl3218 : LedDriver
+    {
+        SMBusDevice dev;
+        /// <summary>
+        /// Construct a new IS31FL3218
+        /// </summary>
+        /// <param name="i2c">The I2C peripheral this chip is attached to</param>
+        /// <param name="rateKhz">The frequency, in kHz, that should be used to communicate with the chip</param>
+        public Is31fl3218(I2c i2c, int rateKhz = 100) : base(18, true, true)
+        {
+            dev = new SMBusDevice(0x54, i2c, rateKhz);
+            dev.WriteByteData((byte)Registers.Shutdown, 0x01).Wait();
+            setBrightness(0);
+
+        }
+
+        bool shutdown;
+
+        /// <summary>
+        /// Gets or sets whether the chip should be in shutdown mode.
+        /// </summary>
+        public bool Shutdown
+        {
+            get { return shutdown;  }
+            set
+            {
+                if (shutdown == value) return;
+                shutdown = value;
+
+                if(shutdown)
+                    dev.WriteByteData((byte)Registers.Shutdown, 0x00).Wait();
+                else
+                    dev.WriteByteData((byte)Registers.Shutdown, 0x01).Wait();
+            }
+
+        }
+
+        private byte[] currentValues = new byte[18];
+
+        private bool[] currentStates = new bool[18];
+
+        /// <summary>
+        /// Flush the data out to the LED driver
+        /// </summary>
+        /// <param name="force">Whether the data should be sent even if the data does not appear to have changed</param>
+        /// <returns>An awaitable task</returns>
+        public override Task Flush(bool force = false)
+        {
+            var states = new byte[3];
+            for (int i = 0; i<currentStates.Length;i++)
+            {
+                var bit = i % 6;
+                var theByte = i / 6;
+                if (currentStates[i])
+                    states[theByte] |= (byte)(1 << bit);
+                else
+                    states[theByte] &= (byte)~(1 << bit);
+            }
+
+            var dataToWrite = currentValues.Concat(states).Concat(new byte[1] { 0x00 }).ToArray();
+            return dev.WriteBufferData((byte)Registers.PwmBase, dataToWrite);
+        }
+
+        internal override void LedBrightnessChanged(Led led)
+        {
+            currentValues[led.Channel] = (byte)Math.Round(led.Brightness * 255);
+            if (AutoFlush) Flush().Wait();
+        }
+
+        internal override void LedStateChanged(Led led)
+        {
+            currentStates[led.Channel] = led.State;
+            if (AutoFlush) Flush().Wait();
+        }
+
+        internal override void setBrightness(double brightness)
+        {
+            bool savedAutoflushState = AutoFlush;
+            AutoFlush = false;
+            foreach (var led in Leds)
+                led.Brightness = brightness;
+            Flush().Wait();
+            AutoFlush = savedAutoflushState;
+        }
+
+        enum Registers
+        {
+            Shutdown = 0x00,
+            PwmBase = 0x01,
+            LedControl1 = 0x13,
+            LedControl2 = 0x14,
+            LedControl3 = 0x15,
+            Update = 0x16,
+            Reset = 0x17
+
+        }
+    }
+}
