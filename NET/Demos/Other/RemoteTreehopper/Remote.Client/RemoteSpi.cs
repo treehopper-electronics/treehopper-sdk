ab5f3e85658454e8eece0f9ae9148b077d96619b
Jay Carlson <jcarlson@unl.edu>
2016-12-27 14:49:06 -0600

    Added remote SPI

---
+++ NET/Demos/Other/RemoteTreehopper/Remote.Client/RemoteSpi.cs
@@ -0,0 +1,50 @@
+﻿using Remote.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treehopper;
+
+namespace Remote.Client
+{
+    public class RemoteSpi : Spi
+    {
+        private RemoteTreehopper board;
+        TaskCompletionSource<byte[]> dataReceived = new TaskCompletionSource<byte[]>();
+        public RemoteSpi(RemoteTreehopper board)
+        {
+            this.board = board;
+            board.RegisterCallback("spi/receive", (msg) =>
+            {
+                var base64string = msg.Message;
+                dataReceived.TrySetResult(Convert.FromBase64String(base64string));
+            });
+        }
+        private bool enabled;
+        public bool Enabled
+        {
+            get
+            {
+                return enabled;
+            }
+
+            set
+            {
+                if (enabled == value) return;
+                enabled = value;
+                board.Write("spi/enabled", enabled);
+            }
+        }
+
+        public async Task<byte[]> SendReceive(byte[] dataToWrite, SpiChipSelectPin chipSelect = null, ChipSelectMode chipSelectMode = ChipSelectMode.SpiActiveLow, double speedMhz = 1, BurstMode burstMode = BurstMode.NoBurst, SpiMode spiMode = SpiMode.Mode00)
+        {
+            var transaction = new SpiTransaction() { DataToWrite = dataToWrite, ChipSelectPinNumber = chipSelect.PinNumber, ChipSelectMode = chipSelectMode, Burst = burstMode, Speed = speedMhz, SpiMode = spiMode };
+            board.Write("spi/send", transaction.ToJsonString());
+            if (burstMode == BurstMode.BurstTx)
+                return new byte[0];
+
+            return await dataReceived.Task;
+        }
+    }
+}
