d69345356ad26fe5bc0c69910c6d4816b1c95039
Jay Carlson <jcarlson@unl.edu>
2016-12-27 13:31:23 -0600

    Explicit board scanning
    Also: introduce a Remote.Shared project to hold strongly-typed classes
    for exchanging data back and forth between client and server via JSON
    (when performance isn't a concern)

---
+++ NET/Demos/Other/RemoteTreehopper/Remote.Shared/RemoteBoard.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treehopper;
+
+namespace Remote.Shared
+{
+    public class RemoteBoardInfo
+    {
+        public RemoteBoardInfo()
+        {
+
+        }
+        public RemoteBoardInfo(TreehopperUsb board)
+        {
+            this.Name = board.Name;
+            this.Serial = board.SerialNumber;
+        }
+
+        public RemoteBoardInfo(string json)
+        {
+            var board = JsonConvert.DeserializeObject<RemoteBoardInfo>(json);
+            this.Name = board.Name;
+            this.Serial = board.Serial;
+        }
+        public string Name { get; set; }
+        public string Serial { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " (" + Serial + ")";
+        }
+
+        public string ToJsonString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
