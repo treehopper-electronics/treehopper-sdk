f2857cea692aa7214efe5900037ee039a1b6ae6a
Jay Carlson <jcarlson@unl.edu>
2017-01-15 22:50:14 -0600

    added docs + CIE brightness calculation

--- NET/Libraries/Treehopper.Libraries/Displays/Led.cs
+++ NET/Libraries/Treehopper.Libraries/Displays/Led.cs
@@ -9,6 +9,11 @@ namespace Treehopper.Libraries.Displays
     /// <summary>
     /// Represents a single LED that may or may not have brightness control
     /// </summary>
+    /// <remarks>
+    /// <para>The default <see cref="State"/> of a newly-constructed LED is "false" (off) with a <see cref="Brightness"/> of 1.0 (even if this LED does not support brightness control). This allows LEDs to be used with many non-dimmable display classes (such as <see cref="SevenSegmentDigit"/>, or <see cref="BarGraph"/>) that only control the LED state. Note that for an LED to be on, its state must be "true" and its brightness must be non-zero. </para>
+    /// <para>Do not confuse the <see cref="State"/>'s value with the electrical value on the pin; most LED drivers are open-drain, thus a "true" state — i.e., when the LED is on — actually corresponds to the pin being driven to 0 (logic false); this is handled by the individual LED drivers to remove ambiguity. If your driver supports driving LEDs in either common-anode (open-drain) or common-cathode configurations, ensure the driver itself is configured to match your circuit.</para>
+    /// <para>You can also attach LEDs to pins that don't belong to LED drivers; see <see cref="GpioLedDriver{TDigitalOutPin}"/> and <see cref="PwmLedDriver{TPwm}"/>, which perform the necessary conversions.</para>
+    /// </remarks>
     public class Led
     {
         /// <summary>
@@ -39,6 +44,10 @@ namespace Treehopper.Libraries.Displays
         /// <summary>
         /// Gets or sets the brightness (0.0 - 1.0) of the LED (if it has brightness control)
         /// </summary>
+        /// <remarks>
+        /// <para>The brightness is perceptual on a linear scale; i.e., a brightness of 0.5 will be perceived as half as bright as a brightness of 1.0.</para>
+        /// <para>Note that the LED will only illuminate if the <see cref="State"/> is true, regardless of the value of this property.</para>
+        /// </remarks>
         public double Brightness
         {
             get { return brightness; }
@@ -62,6 +71,9 @@ namespace Treehopper.Libraries.Displays
         /// <summary>
         /// Gets or sets the state of the LED
         /// </summary>
+        /// <remarks>
+        /// <para>Note that if the LED has brightness control, it will only illuminate if the <see cref="Brightness"/> of the LED is also non-zero.</para>
+        /// </remarks>
         public bool State
         {
             get { return state; }
@@ -71,6 +83,21 @@ namespace Treehopper.Libraries.Displays
                 state = value;
                 Driver.LedStateChanged(this);
             }
+        }
+
+        /// <summary>
+        /// Returns the CIE 1976 relative luminance, normalized to a 0-1 scale.
+        /// </summary>
+        /// <returns>The CIE 1976 relative luminance</returns>
+        /// <remarks>
+        /// <para>The <see cref="Brightness"/> of the LED is perception oriented for intuitiveness. This function can be used to convert the perceptual value to a normalized luminance value that an LED driver can use to determine the correct PWM duty cycle or analog current drive strength needed to produce the associated brightness.</para>
+        /// </remarks>
+        public double GetCieLuminance()
+        {
+            if (Brightness > 0.008856)
+                return (15625 * Math.Pow(Brightness, 3) + 7500 * Math.Pow(Brightness, 2) + 1200 * Brightness + 64) / 24389.0;
+            else
+                return 1000 * Brightness / 9033.0;
         }
     }
 }
