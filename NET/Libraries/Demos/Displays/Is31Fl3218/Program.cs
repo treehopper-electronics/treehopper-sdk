33a39d59ac2b5300b6c29754c8c8d18280b31146
Jay Carlson <jcarlson@unl.edu>
2017-01-16 21:46:50 -0600

    Added IS331FL3218 demo

---
+++ NET/Libraries/Demos/Displays/Is31Fl3218/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treehopper;
+using Treehopper.Libraries.Displays;
+
+namespace Is31Fl3218Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App();
+        }
+
+        static async Task App()
+        {
+            var board = await ConnectionService.Instance.GetFirstDeviceAsync();
+            await board.ConnectAsync();
+
+            var display = new Is31fl3218(board.I2c);
+            display.AutoFlush = false;
+            List<RgbLed> Leds = new List<RgbLed>();
+            for (int i = 0; i < 6; i++)
+                Leds.Add(new RgbLed(display.Leds.Skip(3 * i).Take(3).Reverse().ToList())
+                {
+                    AutoFlush = false,
+                    BlueGain = 0.4,
+                });
+
+            while(!Console.KeyAvailable)
+            {
+                for(int i=0;i<360;i+=6)
+                {
+                    for(int j=0;j<6;j++)
+                    {
+                        Leds[j].SetHsl(i + 30 * j % 360, 100, 50);
+                    }
+                    await display.Flush();
+                    await Task.Delay(25);
+                }
+            }
+        }
+    }
+}
