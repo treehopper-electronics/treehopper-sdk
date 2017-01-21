4501da0f9784ab351ef8e6e089e42b192f5d23a5
Jay Carlson <jcarlson@unl.edu>
2017-01-21 17:46:44 -0600

    Add DM632 project

---
+++ NET/Libraries/Demos/Displays/Dm632/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treehopper;
+using Treehopper.Libraries.Displays;
+
+namespace Dm632Demo
+{
+    /// <summary>
+    /// This program will fade between each of the 16 LED outputs
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App();
+        }
+
+        static async Task App()
+        {
+            var board = await ConnectionService.Instance.GetFirstDeviceAsync();
+            await board.ConnectAsync();
+
+            var driver = new Dm632(board.Spi, board.Pins[5], 6);
+            await driver.Clear();
+
+            var fader1 = new LedFadeAnimation(true);
+            var fader2 = new LedFadeAnimation(true);
+            fader1.Duration = 500;
+            fader2.Duration = 500;
+            while (!Console.KeyAvailable)
+            {
+                for (int i = 0; i < 16; i++)
+                {
+                    fader2.Led = fader1.Led;
+                    fader1.Led = driver.Leds[i];
+
+                    if (fader2.Led != null)
+                        fader2.RunAsync(1, 0).Forget();
+                    await fader1.RunAsync(0, 1);
+                }
+            }
+            
+
+            board.Disconnect();
+        }
+    }
+}
