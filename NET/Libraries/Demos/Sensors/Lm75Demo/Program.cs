fc338395c8e8535ad44893008c61b75a2e755fd0
Jay Carlson <jcarlson@unl.edu>
2017-01-22 01:21:52 -0600

    Add LM75 temperature sensor

---
+++ NET/Libraries/Demos/Sensors/Lm75Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treehopper;
+using Treehopper.Libraries.Sensors.Temperature;
+
+namespace Lm75Demo
+{
+    /// <summary>
+    /// This demo prints current temperature data every second
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App();
+        }
+
+        static async Task App()
+        {
+            var board = await ConnectionService.Instance.GetFirstDeviceAsync();
+            await board.ConnectAsync();
+            var temp = new Lm75(board.I2c);
+
+            Console.WriteLine("Starting temperature reading. Press any key to stop.");
+
+            while (!Console.KeyAvailable)
+            {
+                Console.WriteLine(string.Format("Current temperature: {0:0.00} Celsius ({1:0.00} Fahrenheit)", temp.TemperatureCelsius, temp.TemperatureFahrenheit));
+                await Task.Delay(1000);
+            }
+
+            board.Disconnect();
+            Console.WriteLine("Board disconnected");
+        }
+    }
+}
---
+++ NET/Libraries/Treehopper.Libraries/Sensors/Temperature/Lm75.cs
@@ -0,0 +1,38 @@
+﻿namespace Treehopper.Libraries.Sensors.Temperature
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// LM75 I2c temperature sensor library
+    /// </summary>
+    /// <remarks>
+    /// <para>This library should work with all variants of the ubiquitous LM75, manufactured by NXP, ST, Texas Instruments, and Maxim. Note that some of these variants provide 0.5 degree Celsius resolution, while others provide as high as 0.125 degree resolution.</para>
+    /// </remarks>
+    public class Lm75 : TemperatureSensor
+    {
+        private SMBusDevice dev;
+
+        /// <summary>
+        /// Construct a new LM75 temperature sensor
+        /// </summary>
+        /// <param name="i2c">The i2C port to use</param>
+        /// <param name="a0">the state of the A0 pin</param>
+        /// <param name="a1">the state of the A1 pin</param>
+        /// <param name="a2">the state of the A2 pin</param>
+        public Lm75(I2c i2c, bool a0 = false, bool a1 = false, bool a2 = false)
+        {
+            dev = new SMBusDevice((byte)(0x48 | (a0 ? 1 : 0) | ((a1 ? 1 : 0) << 1) | ((a2 ? 1 : 0) << 2)), i2c);
+            dev.WriteByte(0x00);
+        }
+
+        /// <summary>
+        /// Force an update of the LM75 temperature sensor
+        /// </summary>
+        /// <returns>An awaitable task</returns>
+        public override async Task Update()
+        {
+            short data = (short)await dev.ReadWord();
+            TemperatureCelsius = (data / 32.0) / 8.0;
+        }
+    }
+}
