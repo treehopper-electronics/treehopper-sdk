f40221298d1df85ebfc678e361cf3b4f022402cb
Jay Carlson <jcarlson@unl.edu>
2017-01-22 17:25:46 -0600

    Add support for BME280 and BMP280

---
+++ NET/Libraries/Demos/Sensors/BarometricPressure/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treehopper;
+using Treehopper.Libraries.Sensors.Pressure;
+
+namespace BarometricPressure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App();
+        }
+
+        static async Task App()
+        {
+            var board = await ConnectionService.Instance.GetFirstDeviceAsync();
+            await board.ConnectAsync();
+
+            var sensor = new Bme280(board.I2c);
+            //var sensor = new Bmp280(board.I2c);
+
+            sensor.AutoUpdateWhenPropertyRead = false;
+
+            Console.WriteLine("Press any key to disconnect");
+
+            while(!Console.KeyAvailable)
+            {
+                await sensor.Update();
+                Console.WriteLine(string.Format("Pressure:    {0:0.00} Atm", sensor.Atm));
+                Console.WriteLine(string.Format("Altitude:    {0:0.00} m", sensor.Altitude));
+                Console.WriteLine(string.Format("Temperature: {0:0.00} Celsius", sensor.Celsius));
+                
+                // comment this line out if you're not using a sensor with humidity measurement
+                Console.WriteLine(string.Format("Humidity:    {0:0.00} % RH", sensor.RelativeHumidity));
+                Console.WriteLine();
+                await Task.Delay(1000);
+            }
+
+            Console.WriteLine("Board disconnected");
+        }
+    }
+}
---
+++ NET/Libraries/Treehopper.Libraries/Sensors/Pressure/PressureSensor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treehopper.Libraries.Sensors.Pressure
+{
+    public abstract class PressureSensor : IPressure
+    {
+        public bool AutoUpdateWhenPropertyRead { get; set; }
+
+        public double Pascal { get; protected set; }
+
+        public double Bar { get { return Pascal / 100000.0; } }
+
+        public double Atm { get { return Pascal / (1.01325 * 100000.0); } }
+
+        public double Psi { get { return Atm / 14.7; } }
+
+        public abstract Task Update();
+    }
+}
