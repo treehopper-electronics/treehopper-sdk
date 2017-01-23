7ed561b0f0beaec6ed9f9817fc1a80f7fef443a7
Jay Carlson <jcarlson@unl.edu>
2017-01-22 19:57:45 -0600

    Add MCP4725 library + demo

---
+++ NET/Libraries/Demos/Interface/DacDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treehopper;
+using Treehopper.Libraries.Interface.Dac;
+
+namespace DacDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App();
+        }
+
+        static async Task App()
+        {
+            var board = await ConnectionService.Instance.GetFirstDeviceAsync();
+            await board.ConnectAsync();
+
+            var dac = new Mcp4725(board.I2c);
+            Stopwatch sw = new Stopwatch();
+            Console.WriteLine("Outputing sine wave. Press any key to stop");
+            sw.Start();
+            double frequency = 10;
+            while(!Console.KeyAvailable)
+            {
+                dac.Value = Math.Sin(2.0 * Math.PI * (frequency * sw.Elapsed.TotalMilliseconds / 1000.0)) / 2.0 + 0.5;
+            }
+            Console.WriteLine("Disconnected");
+        }
+    }
+}
---
+++ NET/Libraries/Treehopper.Libraries/Interface/Dac/IDac.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treehopper.Libraries.Interface.Dac
+{
+    public interface IDac
+    {
+        double Value { get; set; }
+
+        double Voltage { get; set; }
+
+        int DacValue { get; set; }
+    }
+}
