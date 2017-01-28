f558cd97825756831be4d2f5af8349042d3ad1fd
Jay Carlson <jcarlson@unl.edu>
2017-01-28 17:09:21 -0600

    Add RGB LED interface to support RGB-specific LEDs

---
+++ NET/Libraries/Treehopper.Libraries/Displays/IRgbLed.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treehopper.Libraries.Displays
+{
+    interface IRgbLed
+    {
+        float RedGain { get; set; }
+        float GreenGain { get; set; }
+        float BlueGain { get; set; }
+
+        void SetRgb(Color color);
+        void SetRgb(float red, float green, float blue);
+        void SetHsl(float hue, float saturation, float luminance);
+    }
+}
