e179acb4b517d26b4a413550a5d3d34933a9c2f0
Jay Carlson <jcarlson@unl.edu>
2017-02-07 22:50:25 -0600

    Support arbitrary-length strings

--- NET/Libraries/Demos/Displays/Apa102Demo/Program.cs
+++ NET/Libraries/Demos/Displays/Apa102Demo/Program.cs
@@ -17,11 +17,11 @@ namespace Apa102Demo
 
         static async Task App2()
         {
-            var board = ConnectionService.Instance.Boards.Where(b =>b.Name == "APA102").FirstOrDefault();
+            var board = await ConnectionService.Instance.GetFirstDeviceAsync();
             await board.ConnectAsync();
             Console.WriteLine("Board connected. Starting rainbow demo");
             Console.WriteLine("Press any key to clear display and disconnect.");
-            var driver = new Apa102(board.Spi, 28);
+            var driver = new Apa102(board.Spi, 300); // change "300" to match the number of LEDs you have connected
 
             driver.Brightness = 1; 
             driver.AutoFlush = false; // only send updates when we explicitly Flush()
@@ -32,13 +32,13 @@ namespace Apa102Demo
             {
                 for (int i = 0; i < driver.Leds.Count; i++)
                 {
-                    driver.Leds[i].SetHsl(i * 12 + hueOffset, 100, 50);
+                    driver.Leds[i].SetHsl((360f / driver.Leds.Count ) * i + hueOffset, 100, 50);
                 }
 
                 await driver.Flush();
-                await Task.Delay(10);
+                //await Task.Delay(10);
 
-                hueOffset += 1;
+                hueOffset += 3;
             }
 
             await driver.Clear();
