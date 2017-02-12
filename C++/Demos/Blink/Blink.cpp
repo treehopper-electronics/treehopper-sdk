cf96119dc27871931b1ceb6dcfbba847a9bafb3b
Jay Carlson <jcarlson@unl.edu>
2017-02-12 02:09:13 -0600

    Add I2cDeviceScanner C++ demo

--- C++/Demos/Blink/Blink.cpp
+++ C++/Demos/Blink/Blink.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 
 using namespace std;
+using namespace Treehopper;
 
 int main()
 {
---
+++ C++/Demos/I2cDeviceScanner/I2cDeviceScanner.cpp
@@ -0,0 +1,41 @@
+// I2cDeviceScanner.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "ConnectionService.h"
+#include "I2cTransferException.h"
+#include <iostream>
+#include "Windows.h"
+#include "Settings.h"
+using namespace std;
+using namespace Treehopper;
+
+int main()
+{
+	ConnectionService service;
+	TreehopperUsb& board = *service.boards[0];
+	Settings::getInstance().throwExceptions = true;
+	board.connect();
+	board.i2c.setEnabled(true);
+	for (int i = 1; i < 127; i++) {
+		cout << "0x" << std::hex << i << ": ";
+		for (int j = 0; j < 3; j++)	{
+			try {
+				uint8_t sendData = 0;
+				board.i2c.sendReceive((uint8_t)i, &sendData, 1, NULL, 0);
+				cout << "Device found!";
+				break;
+			} catch (...) {
+				cout << "...";
+			}
+
+			Sleep(10);
+		}
+		cout << endl;
+	}
+	board.disconnect();
+	cout << "Scan complete. Press any key to close." << endl;
+	cin.get();
+    return 0;
+}
+
