8b5057afb1b4213f9b61e31c4b1e2b6c1e77b91f
Jay Carlson <jcarlson@unl.edu>
2017-02-12 02:08:44 -0600

    Added LM75 temperature sensor

---
+++ C++/API/Treehopper.Libraries/inc/Sensors/Temperature/Lm75.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Treehopper.Libraries/inc/Treehopper.Libraries.h"
+#include "TemperatureSensor.h"
+#include "Treehopper/inc/I2c.h"
+#include "Treehopper/inc/SMBusDevice.h"
+#include <memory>
+namespace Treehopper
+{
+	namespace Libraries
+	{
+		namespace Sensors
+		{
+			namespace Temperature
+			{
+				class LIBRARIES_API Lm75 : public TemperatureSensor
+				{
+				public:
+					Lm75(I2c* i2c, bool a0 = false, bool a1 = false, bool a2 = false);
+					~Lm75();
+					void update();
+				private:
+					unique_ptr<SMBusDevice> dev;
+				};
+			}
+		}
+	}
+}
