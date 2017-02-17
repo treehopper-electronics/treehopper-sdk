21966ebe624e7b5ca7f6cf4e563bd20caf737f20
Jay Carlson <jcarlson@unl.edu>
2017-02-17 00:35:16 -0600

    Add RotaryEncoder

---
+++ NET/Libraries/Demos/Input/RotaryEncoder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treehopper;
+using Treehopper.Libraries.Input;
+
+namespace RotaryEncoderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App();
+        }
+
+        static async Task App()
+        {
+            var board = await ConnectionService.Instance.GetFirstDeviceAsync();
+            await board.ConnectAsync();
+
+            var encoder = new RotaryEncoder(board.Pins[0], board.Pins[1], 4);
+            encoder.MinValue = 0;
+            encoder.MaxValue = 19;
+            encoder.PositionChanged += Encoder_PositionChanged;
+        }
+
+        private static void Encoder_PositionChanged(object sender, RotaryEncoder.PositionChangedEventArgs e)
+        {
+            Console.WriteLine(string.Format("New position: {0}",e.NewPosition));
+        }
+    }
+}
