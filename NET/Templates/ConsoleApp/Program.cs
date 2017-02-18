9ec9e6e90350cb4dcaaa7f406062f5975f23c13a
Jay Carlson <jcarlson@unl.edu>
2017-02-18 17:47:19 -0600

    Add template projects

---
+++ NET/Templates/ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treehopper;
+using Treehopper.Desktop;
+using Treehopper.Libraries;
+
+namespace $safeprojectname$
+{
+    class Program
+    {
+        static TreehopperUsb board;
+        static void Main(string[] args)
+        {
+            App().Wait();
+            board?.Disconnect();
+            Console.WriteLine("Board disconnected");
+        }
+
+        static async Task App()
+        {
+            board = await ConnectionService.Instance.GetFirstDeviceAsync();
+            await board.ConnectAsync();
+            Console.WriteLine("Connected to: " + board);
+            Console.WriteLine("Firmware version: " + board.VersionString);
+            
+            // Write your code here
+
+        }
+    }
+}
