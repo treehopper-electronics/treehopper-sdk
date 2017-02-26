edc733628aad5aeee9a822ba3af64cf5f2ec2887
Jay Carlson <jcarlson@unl.edu>
2017-02-26 00:48:45 -0600

    Add docs

--- C++/API/Treehopper/inc/I2c.h
+++ C++/API/Treehopper/inc/I2c.h
@@ -23,7 +23,13 @@ namespace Treehopper
 		/** Gets whether the module is enabled */
 		virtual bool enabled() = 0;
 
-		/** Send and/or receive data from the module */
+		/** Send and/or receive data with the i2c module.
+		@param[in] address the address of the slave i2c board you wish to communicate with
+		@param[in] writeBuffer a pointer to the buffer of data to send
+		@param[in] numBytesToWrite the number of bytes to write
+		@param[out] readBuffer a pointer to the buffer to use for reading data into
+		@param[in] numBytesToRead the number of bytes to read from the board
+		*/
 		virtual void sendReceive(uint8_t address, uint8_t* writeBuffer, size_t numBytesToWrite,
 			uint8_t* readBuffer = NULL, size_t numBytesToRead = 0) = 0;
 	};
