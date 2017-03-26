6427fb7065f29281e14136b18e9776b8a83d76d6
Jay Carlson <jcarlson@unl.edu>
2017-03-26 15:11:50 -0500

    Added NES and SNES controllers

---
+++ NET/Libraries/Treehopper.Libraries/Input/NesController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treehopper.Libraries.Interface.PortExpander;
+using Treehopper.Libraries.Sensors;
+
+namespace Treehopper.Libraries.Input
+{
+    public class NesController : IPollable
+    {
+        protected SpiDevice dev;
+
+        public Button A { get; private set; }
+        public Button B { get; private set; }
+
+        public Button Start { get; private set; }
+        public Button Select { get; private set; }
+
+        public event DPadStateEventHandler DPadStateChanged;
+
+        protected DPadState dpad;
+        public DPadState DPad
+        {
+            get
+            {
+                if (AutoUpdateWhenPropertyRead) Update().Wait();
+                return dpad;
+            }
+        }
+
+        public NesController(Spi spi, SpiChipSelectPin ps)
+        {
+            this.dev = new SpiDevice(spi, ps, ChipSelectMode.PulseHighAtBeginning);
+
+            A = new Button(new DigitalInPeripheralPin(this));
+            B = new Button(new DigitalInPeripheralPin(this));
+            Start = new Button(new DigitalInPeripheralPin(this));
+            Select = new Button(new DigitalInPeripheralPin(this));
+        }
+
+        public bool AutoUpdateWhenPropertyRead { get; set; } = true;
+
+        public int AwaitPollingInterval { get; set; } = 20;
+
+        public virtual async Task Update()
+        {
+            var result = await dev.SendReceive(new byte[] { 0x00 });
+            BitArray values = new BitArray(result);
+            ((DigitalInPeripheralPin)A.Input).DigitalValue = values[7];
+            ((DigitalInPeripheralPin)B.Input).DigitalValue = values[6];
+            ((DigitalInPeripheralPin)Select.Input).DigitalValue = values[5];
+            ((DigitalInPeripheralPin)Start.Input).DigitalValue = values[4];
+
+            var oldState = dpad;
+
+            if (!values[3])
+                dpad = DPadState.Up;
+            else if (!values[2])
+                dpad = DPadState.Down;
+            else if (!values[1])
+                dpad = DPadState.Left;
+            else if (!values[0])
+                dpad = DPadState.Right;
+            else
+                dpad = DPadState.None;
+
+            if (oldState != dpad)
+                RaiseDPadChanged();
+        }
+
+        protected void RaiseDPadChanged()
+        {
+            DPadStateChanged?.Invoke(this, new DPadStateEventArgs() { NewValue = dpad });
+        }
+    }
+}
