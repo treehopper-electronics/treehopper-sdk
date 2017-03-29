f9a5b8f4c5bf143e44bb8359aa4207cd476fd5d2
Jay Carlson <jcarlson@unl.edu>
2017-03-28 21:58:42 -0500

    Java project structure refactoring

--- Java/demos/desktop/sandbox/src/main/java/io/treehopper/desktop/demos/sandbox/Sandbox.java
+++ Java/demos/desktop/sandbox/src/main/java/io/treehopper/desktop/demos/sandbox/Sandbox.java
@@ -5,6 +5,9 @@ import io.treehopper.desktop.*;
 import io.treehopper.*;
 import io.treehopper.libraries.displays.SevenSegmentDisplay;
 import io.treehopper.libraries.displays.Tm1650;
+import io.treehopper.libraries.input.RotaryEncoder;
+import io.treehopper.libraries.motors.SeeedGroveI2cMotorDriver;
+import io.treehopper.libraries.sensors.temperature.Mcp9808;
 
 public class Sandbox {
     public static void main(String[] args) {
@@ -13,9 +16,19 @@ public class Sandbox {
         TreehopperUsb board = boards.get(0);
         board.connect();
 
-        Tm1650 driver = new Tm1650(board.i2c);
-        SevenSegmentDisplay display = new SevenSegmentDisplay(driver.leds, false);
-        display.setText("1234");
-        board.disconnect();
+        SeeedGroveI2cMotorDriver driver = new SeeedGroveI2cMotorDriver(board.i2c);
+
+        driver.setMotor1(1.0);
+
+        while(true)
+        {
+//            System.out.println(temp.getTemperatureFahrenheit());
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+        }
+
     }
 }
