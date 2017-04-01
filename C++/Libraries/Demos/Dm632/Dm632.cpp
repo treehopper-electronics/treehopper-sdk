4bdc44693ebf33cbf1e50c9a3c8fffcc7f1213fe
Jay Carlson <jcarlson@unl.edu>
2017-04-01 12:06:17 -0500

    We have to open the device to fill in the name property

--- C++/Libraries/Demos/Dm632/Dm632.cpp
+++ C++/Libraries/Demos/Dm632/Dm632.cpp
@@ -5,8 +5,6 @@
 using namespace Treehopper;
 using namespace Treehopper::Libraries::Displays;
 
-uint8_t dataToSend[32];
-
 int main()
 {
 	TreehopperUsb& board = ConnectionService::instance().boards[0];
@@ -16,24 +14,23 @@ int main()
 
 	auto driver = Dm632(board.spi, &board.pins[5]);
 
-	dataToSend[0] = 255;
-	dataToSend[1] = 255;
-	dataToSend[30] = 255;
-	dataToSend[31] = 255;
-
-	for(int i=0;i<16;i++)
+	while (true)
 	{
-		driver.leds[i].brightness(0.5);
-		driver.leds[i].state(true);
-		this_thread::sleep_for(100ms);
-	}
+		for (int i = 0; i<16; i++)
+		{
+			driver.leds[i].brightness(0.25);
+			driver.leds[i].state(true);
+			this_thread::sleep_for(100ms);
+		}
 
-	for (int i = 0; i<16; i++)
-	{
-		driver.leds[i].brightness(0);
-		driver.leds[i].state(true);
-		this_thread::sleep_for(100ms);
+		for (int i = 0; i<16; i++)
+		{
+			driver.leds[i].brightness(0);
+			driver.leds[i].state(true);
+			this_thread::sleep_for(100ms);
+		}
 	}
+	
 	
 	board.disconnect();
     return 0;
