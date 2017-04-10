00ef8d0a67889cee7adf15e1dabcc66936d0387b
Jay Carlson <jay.d.carlson@gmail.com>
2017-04-10 10:29:21 -0500

    Tidy up unused code

--- NET/API/Treehopper.Desktop/LibUsb/NativeMethods.cs
+++ NET/API/Treehopper.Desktop/LibUsb/NativeMethods.cs
@@ -18,10 +18,6 @@ namespace Treehopper.Desktop.LibUsb
 		[DllImport(LIBUSB_DLL, CallingConvention = CC, SetLastError = false, EntryPoint = "libusb_init")]
 		internal static extern int Init(out IntPtr pContext);
 
-		// libusb_detach_kernel_driver
-		[DllImport(LIBUSB_DLL, CallingConvention = CC, SetLastError = false, EntryPoint = "libusb_detach_kernel_driver")]
-		internal static extern int DetachKernelDriver(LibUsbDeviceHandle dev, int interface_number);
-
 		// libusb_exit
 		[DllImport(LIBUSB_DLL, CallingConvention = CC, SetLastError = false, EntryPoint = "libusb_exit")]
 		internal static extern void Exit(IntPtr pContext);
@@ -50,29 +46,13 @@ namespace Treehopper.Desktop.LibUsb
 		[DllImport(LIBUSB_DLL, CallingConvention = CC, SetLastError = false, EntryPoint = "libusb_get_string_descriptor_ascii")]
 		public static extern int GetStringDescriptor([In] LibUsbDeviceHandle deviceProfileHandle, [In] byte desc_index, StringBuilder sb, int length);
 
-		[DllImport(LIBUSB_DLL, CallingConvention = CC, SetLastError = false, EntryPoint = "libusb_get_string_descriptor_ascii")]
-		public static extern int GetStringDescriptor([In] LibUsbDeviceHandle deviceProfileHandle, [In] byte desc_index, Byte[] charData, int length);
-
 		// libusb_open
 		[DllImport(LIBUSB_DLL, CallingConvention = CC, SetLastError = false, EntryPoint = "libusb_open")]
 		internal static extern int Open([In] IntPtr deviceProfileHandle, ref IntPtr deviceHandle);
 
-		[DllImport(LIBUSB_DLL, CallingConvention = CC, SetLastError = false, EntryPoint = "libusb_open_device_with_vid_pid")]
-		internal static extern IntPtr Open([In] IntPtr context, ushort vendor_id, ushort product_id);
-
-
-		// libusb_set_configuration
-		[DllImport(LIBUSB_DLL, CallingConvention = CC, SetLastError = false, EntryPoint = "libusb_set_configuration")]
-		public static extern int SetConfiguration([In] LibUsbDeviceHandle deviceHandle, int configurationNumber);
-
 		// libusb_claim_interface
 		[DllImport(LIBUSB_DLL, CallingConvention = CC, SetLastError = false, EntryPoint = "libusb_claim_interface")]
 		public static extern int ClaimInterface([In] LibUsbDeviceHandle deviceHandle, int interfaceNumber);
-
-		// libusb_set_interface_alt_setting
-		[DllImport(LIBUSB_DLL, CallingConvention = CC, SetLastError = false, EntryPoint = "libusb_set_interface_alt_setting")]
-		public static extern int SetInterfaceAltSetting([In] LibUsbDeviceHandle deviceHandle, int interfaceNumber, int alternateSetting);
-
 
 		// libusb_close
 		[DllImport(LIBUSB_DLL, CallingConvention = CC, SetLastError = false, EntryPoint = "libusb_close")]
@@ -81,8 +61,5 @@ namespace Treehopper.Desktop.LibUsb
 		// libusb_bulk_transfer
 		[DllImport(LIBUSB_DLL, CallingConvention = CC, SetLastError = false, EntryPoint = "libusb_bulk_transfer")]
 		internal static extern LibUsbError BulkTransfer([In] LibUsbDeviceHandle deviceHandle, byte endpoint, Byte[] Data, int length, out int actualLength, int timeout);
-
-		[DllImport(LIBUSB_DLL, CallingConvention = CC, SetLastError = false, EntryPoint = "libusb_interrupt_transfer")]
-		public static extern int InterruptTransfer([In] LibUsbDeviceHandle deviceHandle, byte endpoint, Byte[] pData, int length, out int actualLength,	int timeout);
 	}
 }
