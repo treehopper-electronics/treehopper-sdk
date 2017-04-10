2ab2bb31b1c5d68e6e56dd6e5c8daa436a8a1b1d
Jay Carlson <jay.d.carlson@gmail.com>
2017-04-09 19:29:44 -0500

    Initial LibUSB work
    No names/serial numbers yet

---
+++ NET/API/Treehopper.Desktop/LibUsb/LibUsbConnection.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace Treehopper.Desktop.LibUsb
+{
+	public class LibUsbConnection : IConnection
+	{
+		const byte pinReportEndpoint = 0x81;
+		const byte peripheralResponseEndpoint = 0x82;
+		const byte pinConfigEndpoint = 0x01;
+		const byte peripheralConfigEndpoint = 0x02;
+
+		LibUsbDeviceHandle deviceHandle;
+		IntPtr deviceProfile;
+
+
+		public LibUsbConnection(IntPtr deviceProfile)
+		{
+			this.deviceProfile = deviceProfile;
+		}
+
+		public string DevicePath { get; private set; }
+
+		public string Name { get; private set; }
+
+		public string Serial { get; private set; }
+
+		public int UpdateRate { get; set; } = 1000;
+
+		public short Version { get; private set; }
+
+		public event PinEventData PinEventDataReceived;
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void Close()
+		{
+			deviceHandle.Dispose();
+		}
+
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<bool> OpenAsync()
+		{
+			IntPtr handle = new IntPtr();
+			NativeMethods.Open(deviceProfile, ref handle);
+			this.deviceHandle = new LibUsbDeviceHandle(handle);
+			return true;
+		}
+
+		public async Task<byte[]> ReadPeripheralResponsePacket(uint numBytesToRead)
+		{
+			byte[] data = new byte[numBytesToRead];
+			int len = 0;
+			NativeMethods.BulkTransfer(deviceHandle, pinConfigEndpoint, data, (int)numBytesToRead, out len, 1000);
+			return data;
+		}
+
+		public async Task SendDataPeripheralChannel(byte[] data)
+		{
+			int len = 0;
+			NativeMethods.BulkTransfer(deviceHandle, peripheralConfigEndpoint, data, data.Length, out len, 1000);
+		}
+
+		public async Task SendDataPinConfigChannel(byte[] data)
+		{
+			int len = 0;
+			NativeMethods.BulkTransfer(deviceHandle, pinConfigEndpoint, data, data.Length, out len, 1000);
+		}
+	}
+}
---
+++ NET/API/Treehopper.Desktop/LibUsb/LibUsbDeviceHandle.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Treehopper.Desktop.LibUsb
+{
+	public class LibUsbDeviceHandle : SafeHandle
+	{
+		//private static Object handleLOCK = new object();
+		//private static LibUsbError mLastReturnCode;
+		//private static String mLastReturnString = String.Empty;
+
+
+		///// If the device handle is <see cref="SafeContextHandle.IsInvalid"/>, gets a descriptive string for the <see cref="LastErrorCode"/>.
+		///// </summary>
+		//public static string LastErrorString
+		//{
+		//	get
+		//	{
+		//		lock (handleLOCK)
+		//		{
+		//			return mLastReturnString;
+		//		}
+		//	}
+		//}
+		///// <summary>
+		///// If the device handle is <see cref="SafeContextHandle.IsInvalid"/>, gets the <see cref="LibUsbError"/> status code indicating the reason.
+		///// </summary>
+		//public static LibUsbError LastErrorCode
+		//{
+		//	get
+		//	{
+		//		lock (handleLOCK)
+		//		{
+		//			return mLastReturnCode;
+		//		}
+		//	}
+		//}
+
+		public override bool IsInvalid
+		{
+			get
+			{
+				if (handle != IntPtr.Zero)
+				{
+					return (handle == new IntPtr(-1));
+				}
+				return true;
+			}
+		}
+
+		//public LibUsbDeviceHandle(LibUsbProfileHandle profileHandle)
+		//{
+		//	IntPtr pDeviceHandle = IntPtr.Zero;
+		//	int ret = LibUsbApi.Open(profileHandle, ref pDeviceHandle);
+		//	if (ret < 0 || pDeviceHandle == IntPtr.Zero)
+		//	{
+		//		lock (handleLOCK)
+		//		{
+		//			mLastReturnCode = (LibUsbError)ret;
+		//			mLastReturnString = LibUsbApi.StrError(mLastReturnCode);
+		//		}
+		//		SetHandleAsInvalid();
+		//	}
+		//	else
+		//	{
+		//		SetHandle(pDeviceHandle);
+		//	}
+
+		//}
+
+		internal LibUsbDeviceHandle(IntPtr pDeviceHandle) : base(pDeviceHandle, true)
+		{
+		}
+		///<summary>
+		///Closes the <see cref="LibUsbDeviceHandle"/>.
+		///</summary>
+		///<returns>
+		///true if the <see cref="LibUsbDeviceHandle"/> is released successfully; otherwise, in the event of a catastrophic failure, false. In this case, it generates a ReleaseHandleFailed Managed Debugging Assistant.
+		///</returns>
+		protected override bool ReleaseHandle()
+		{
+			if (!IsInvalid)
+			{
+				NativeMethods.Close(handle);
+				SetHandleAsInvalid();
+			}
+			return true;
+		}
+
+		/// <summary>
+		/// Closes the <see cref="LibUsbDeviceHandle"/> reference.  When all references are no longer is use, the device
+		/// is closed in the <see cref="ReleaseHandle"/> finalizer.
+		/// </summary>
+		/// <remarks>
+		/// <note title="Libusb-1.0 API Note:" type="cpp">The <see cref="Close"/> method is roughly equivalent to <a href="http://libusb.sourceforge.net/api-1.0/group__dev.html#ga779bc4f1316bdb0ac383bddbd538620e">libusb_close()</a>.</note>
+		/// </remarks>
+		public new void Close()
+		{
+			base.Close();
+		}
+	}
+}
