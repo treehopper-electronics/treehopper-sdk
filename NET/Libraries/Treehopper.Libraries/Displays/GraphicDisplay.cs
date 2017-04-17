b90ee12b8925b257f16ff9fe1c935407d09e660e
Jay Carlson <jcarlson@unl.edu>
2017-04-16 22:06:33 -0500

    Add PCD8544 (Nokia 5110) LCD

--- NET/Libraries/Treehopper.Libraries/Displays/GraphicDisplay.cs
+++ NET/Libraries/Treehopper.Libraries/Displays/GraphicDisplay.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Treehopper.Libraries.Interface;
 
 namespace Treehopper.Libraries.Displays
 {
     /// <summary>
     /// A class representing pixel-addressed displays
     /// </summary>
-    public abstract class GraphicDisplay
+    public abstract class GraphicDisplay : IFlushable
     {
         /// <summary>
         /// The height of the display, in pixels
@@ -35,12 +36,12 @@ namespace Treehopper.Libraries.Displays
         /// <param name="Width">The width, in pixels, of the display</param>
         /// <param name="Height">The height, in pixels, of the display</param>
         /// <param name="bytesPerRow">The number of bytes per row (stride) of the display</param>
-        public GraphicDisplay(int Width, int Height, int bytesPerRow)
+        public GraphicDisplay(int Width, int Height, double bytesPerPixel)
         {
             this.Width = Width;
             this.Height = Height;
 
-            RawBuffer = new byte[bytesPerRow * Height];
+            RawBuffer = new byte[(int)Math.Round(Width * Height * bytesPerPixel)];
         }
 
         /// <summary>
@@ -73,6 +74,10 @@ namespace Treehopper.Libraries.Displays
             }
         }
 
+        public bool AutoFlush { get; set; } = true;
+
+        public IFlushable Parent { get; }
+
         /// <summary>
         /// Sets the global brightness of the graphic display
         /// </summary>
@@ -83,15 +88,16 @@ namespace Treehopper.Libraries.Displays
         /// Flush any updates to the RawBuffer to the display
         /// </summary>
         /// <returns>An awaitable task that completes upon sending the update to the display</returns>
-        public Task Flush()
-        {
-            return flush();
-        }
 
         /// <summary>
         /// Internal function called by the implementation to flush the RawBuffer to the display
         /// </summary>
         /// <returns>An awaitable task that completes upon success</returns>
         protected abstract Task flush();
+
+        public Task Flush(bool force = false)
+        {
+            return flush();
+        }
     }
 }
