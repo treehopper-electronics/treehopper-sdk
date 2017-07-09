692ede246c28c5a6b60818d8d01e0a89e087e918
Jay Carlson <jcarlson@unl.edu>
2017-07-09 02:35:07 -0500

    Use reflection to get plug-ins

--- NET/Other/TreehopperControlCenter/TreehopperControlCenter/ConnectedPage.xaml.cs
+++ NET/Other/TreehopperControlCenter/TreehopperControlCenter/ConnectedPage.xaml.cs
@@ -5,17 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using Treehopper;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 using Xamarin.Forms.Xaml;
 
 namespace TreehopperControlCenter
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ConnectedPage : TabbedPage
+    public partial class ConnectedPage : Xamarin.Forms.TabbedPage
     {
         public ConnectedPage (TreehopperUsb Board)
         {
             Title = Board.ToString();
-
+            On<Xamarin.Forms.PlatformConfiguration.Android>().SetIsSwipePagingEnabled(false);
             Children.Add(new Pages.SignalPage(Board));
             Children.Add(new Pages.LibrariesPage(Board));
 
