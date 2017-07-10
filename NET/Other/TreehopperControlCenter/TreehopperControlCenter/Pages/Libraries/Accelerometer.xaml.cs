c4d877c6ee726489c0864eb9ad551199ceea7dd3
Jay Carlson <jcarlson@unl.edu>
2017-07-09 20:23:05 -0500

    Add Accelerometer and Rotary Encoder library components

---
+++ NET/Other/TreehopperControlCenter/TreehopperControlCenter/Pages/Libraries/Accelerometer.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treehopper;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Treehopper.Libraries;
+using Treehopper.Libraries.Sensors.Inertial;
+
+namespace TreehopperControlCenter.Pages.Libraries
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Accelerometer : LibraryComponent
+    {
+        public string SelectedAccelerometer { get; set; }
+        public bool AddressBitSet { get; set; }
+
+        public string[] Accelerometers { get; set; } = new string[] { "MPU-9250", "ADXL-345", "LIS3DH" };
+
+        public TreehopperUsb Board { get; }
+
+        public IAccelerometer Sensor { get; set; }
+
+        //public double AccelerometerX { get; set; }
+        public double AccelerometerX => Sensor?.Accelerometer.X ?? 0;
+        public double AccelerometerY => Sensor?.Accelerometer.Y ?? 0;
+        public double AccelerometerZ => Sensor?.Accelerometer.Z ?? 0;
+
+        public Accelerometer(LibrariesPage page, TreehopperUsb Board = null) : base("Accelerometer", page)
+        {
+            this.Board = Board;
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        public Accelerometer() : base("Accelerometer", new LibrariesPage())
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        protected override async Task Start()
+        {
+            switch(SelectedAccelerometer)
+            {
+                case "MPU-9250":
+                    Sensor = await Mpu9250.Create(Board.I2c, AddressBitSet).ConfigureAwait(false);
+                    break;
+
+                case "ADXL-345":
+                    Sensor = new Adxl345(Board.I2c, AddressBitSet);
+                    break;
+
+                case "LIS3DH":
+                    Sensor = new Lis3dh(Board.I2c, AddressBitSet);
+                    break;
+            }
+
+            Sensor.AutoUpdateWhenPropertyRead = false;
+            OnPropertyChanged(nameof(Sensor));
+        }
+
+        protected override async Task Stop()
+        {
+            Sensor = null;
+        }
+
+        public override void Dispose()
+        {
+            Sensor = null;
+        }
+
+        public override async Task Update()
+        {
+            if(Sensor != null)
+            {
+                await Sensor.Update().ConfigureAwait(false);
+
+                //AccelerometerX = Sensor.Accelerometer.X;
+
+
+                OnPropertyChanged(nameof(AccelerometerX));
+                OnPropertyChanged(nameof(AccelerometerY));
+                OnPropertyChanged(nameof(AccelerometerZ));
+            }
+                
+        }
+    }
+}
--- NET/Other/TreehopperControlCenter/TreehopperControlCenter/Pages/Libraries/LibraryComponent.cs
+++ NET/Other/TreehopperControlCenter/TreehopperControlCenter/Pages/Libraries/LibraryComponent.cs
@@ -5,22 +5,81 @@ using System.Collections.Specialized;
 using System.Windows.Input;
 using Xamarin.Forms;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace TreehopperControlCenter.Pages.Libraries
 {
-    public class LibraryComponent : ContentView
+    public abstract class LibraryComponent : ContentView, IDisposable
     {
         public LibraryComponent(string title, LibrariesPage Parent)
         {
             this.Title = title;
+            StartButtonText = "Start";
             RemoveCommand = new Command(() =>
             {
-                Parent.Components.Remove(this);
+                if(Parent.Components.Contains(this))
+                    Parent.Components.Remove(this);
+                
+                this.Dispose();
+            });
+
+            StartCommand = new Command(() =>
+            {
+                if(!IsRunning)
+                {
+                    // start
+                    StartButtonText = "Stop";
+                    IsRunning = true;
+                    Start();
+                } else
+                {
+                    StartButtonText = "Start";
+                    IsRunning = false;
+                    Stop();
+                }
             });
         }
 
+        private bool isRunning = false;
+
+        public bool IsRunning
+        {
+            get { return isRunning; }
+            set {
+                isRunning = value;
+                OnPropertyChanged(nameof(IsRunning));
+                OnPropertyChanged(nameof(IsStopped));
+            }
+        }
+
+        public bool IsStopped => !IsRunning;
+
+
         public string Title { get; set; }
 
+        private string startButtonText;
+
+        public string StartButtonText
+        {
+            get { return startButtonText; }
+            set {
+                startButtonText = value;
+                OnPropertyChanged(nameof(StartButtonText));
+            }
+        }
+
+        protected abstract Task Start();
+
+        protected abstract Task Stop();
+
+        public abstract void Dispose();
+
+        public View Configuration { get; set; }
+
         public ICommand RemoveCommand { protected set; get; }
+
+        public ICommand StartCommand { protected set; get; }
+
+        public abstract Task Update();
     }
 }
