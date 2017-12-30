ab33ed243e25116bdb76555c2b7a559f1f73f2be
Jay Carlson <jcarlson@unl.edu>
2017-12-30 13:43:16 -0600

    Add I2C send/receive docs

--- Python/treehopper/api/I2C.py
+++ Python/treehopper/api/I2C.py
@@ -25,4 +25,18 @@ class I2C(ABC):
 
     @abstractmethod
     def send_receive(self, address: int, write_data: bytearray, num_bytes_to_read: int):
+        """
+        Send and receive data between Treehopper and an I2C peripheral.
+
+        This function writes write_data to the I2C peripheral at the specified address, and reads back num_bytes_to_read.
+
+        Args:
+            address: the 7-bit I2C address of the peripheral devices.
+            write_data: a list of bytes to send to the peripheral device.
+            num_bytes_to_read: the number of bytes to read back.
+
+        Returns:
+            A list of bytes read from the peripheral device.
+        
+        """
         pass
