dfcb60eb341b8a3122445415da84855efee87e5b
Jay Carlson <jcarlson@unl.edu>
2018-01-06 14:16:35 -0600

    Use procedural instead of event-driven approach

--- NET/Demos/Console/Blink/Program.cs
+++ NET/Demos/Console/Blink/Program.cs
@@ -17,31 +17,9 @@ namespace Blink
         static void Main(string[] args)
         {
             // We can't do async calls from the Console's Main() function, so we run all our code in a separate async method.
-            ConnectionService.Instance.Boards.CollectionChanged += async (o, s) =>
-            {
-                if(s.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
-                {
-                    Board = (TreehopperUsb)s.NewItems[0];
-                    Console.WriteLine("Found board: " + Board);
-					Console.WriteLine("Version: " + Board.Version);
-
-					// You must explicitly connect to a board before communicating with it
-					await Board.ConnectAsync();
-
-					Console.WriteLine("Start blinking. Press any key to stop.");
-					while (Board.IsConnected && !Console.KeyAvailable)
-					{
-						// toggle the LED.
-						Board.Led = !Board.Led;
-						await Task.Delay(100);
-					}
-                } else if(s.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove){
-                    Console.WriteLine("Board removed");
-                }
-            };
-
-            //RunBlink().Wait();
+            Task.Run(RunBlink).Wait();
         }
+
         static async Task RunBlink()
         {
             while (true)
@@ -63,8 +41,9 @@ namespace Blink
 					await Task.Delay(100);
 				}
 
+                Console.ReadKey();
+                Board.Disconnect();
             }
-
         }
     }
 }
