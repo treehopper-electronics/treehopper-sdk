3395ded6cf159d9b2b718e12779f96e72c69f1d0
Jay Carlson <jcarlson@unl.edu>
2018-01-10 20:49:55 -0600

    updating __all__ includes

--- Python/treehopper/api/__init__.py
+++ Python/treehopper/api/__init__.py
@@ -3,12 +3,12 @@
 This module provides digital and analog I/O, hardware and software PWM, I2C, SPI, and UART support.
 """
 
+from treehopper.api.Pin import Pin, PinMode, AdcPin, DigitalIn, DigitalOut, ReferenceLevel
 from treehopper.api.DeviceCommands import DeviceCommands
 from treehopper.api.HardwarePwm import HardwarePwm
-from treehopper.api.Pin import *
 from treehopper.api.Pwm import Pwm
 from treehopper.api.TreehopperUsb import TreehopperUsb
 from treehopper.api.find_boards import find_boards
 from treehopper.api.I2C import I2C
 
-__all__ = ['find_boards', 'TreehopperUsb', 'DeviceCommands', 'HardwarePwm']
+__all__ = ['find_boards', 'TreehopperUsb', 'DeviceCommands', 'HardwarePwm', 'Pin', 'PinMode', 'AdcPin', 'DigitalIn', 'DigitalOut', 'ReferenceLevel', 'I2C']
