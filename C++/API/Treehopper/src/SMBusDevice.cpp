eb147a2945a4f78fcc969b086ad41e36e7c81ef0
Jay Carlson <jcarlson@unl.edu>
2018-02-23 22:41:37 -0600

    Switch to vectors for I2C stuff in C++

--- C++/API/Treehopper/src/SMBusDevice.cpp
+++ C++/API/Treehopper/src/SMBusDevice.cpp
@@ -22,121 +22,107 @@ namespace Treehopper
 		i2c.speed(rateKhz);
 
 		// S Addr Rd [A] [Data] NA P
-		uint8_t data;
-		i2c.sendReceive(address, NULL, 0, &data, 1);
-		return data;
+		return i2c.sendReceive(address, std::vector<uint8_t>(), 1)[0];
 	}
 
 	void SMBusDevice::writeByte(uint8_t data)
 	{
 		i2c.speed(rateKhz);
+		std::vector<uint8_t> vec = { data };
 
 		// S Addr Wr [A] Data [A] P
-		i2c.sendReceive(address, &data, 1, NULL, 0);
+		i2c.sendReceive(address, vec, 0);
 	}
 
-	void SMBusDevice::writeData(uint8_t* dataOut, size_t count)
+	void SMBusDevice::writeData(std::vector<uint8_t> data)
 	{
 		i2c.speed(rateKhz);
-		i2c.sendReceive(address, dataOut, count, NULL, 0);
+		i2c.sendReceive(address, data, 0);
 	}
 
-	void SMBusDevice::readData(uint8_t* dataIn, size_t count)
+	std::vector<uint8_t> SMBusDevice::readData(size_t count)
 	{
 		i2c.speed(rateKhz);
-		i2c.sendReceive(address, NULL, 0, dataIn, count);
+		return i2c.sendReceive(address, std::vector<uint8_t>(), count);
 	}
 
 	uint8_t SMBusDevice::readByteData(uint8_t reg)
 	{
 		i2c.speed(rateKhz);
-		uint8_t data;
+		std::vector<uint8_t> data = { reg };
 		// S Addr Wr [A] Comm [A] S Addr Rd [A] [Data] NA P
-		i2c.sendReceive(address, &reg, 1, &data, 1);
-		return data;
+		return i2c.sendReceive(address, data, 1)[0];
 	}
 
 	uint16_t SMBusDevice::readWordData(uint8_t reg)
 	{
-		uint8_t data[2];
 		i2c.speed(rateKhz);
+		std::vector<uint8_t> regData = { reg };
 		// S Addr Wr [A] Comm [A] S Addr Rd [A] [DataLow] A [DataHigh] NA P
-		i2c.sendReceive(address, &reg, 1, data, 2);
+		auto data = i2c.sendReceive(address, regData, 2);
 		return (uint16_t)((data[1] << 8) | data[0]);
 	}
 
 	uint16_t SMBusDevice::readWordDataBE(uint8_t reg)
 	{
-		uint8_t data[2];
+		std::vector<uint8_t> regData = { reg };
 		i2c.speed(rateKhz);
 		// S Addr Wr [A] Comm [A] S Addr Rd [A] [DataLow] A [DataHigh] NA P
-		i2c.sendReceive(address, &reg, 1, data, 2);
+		auto data = i2c.sendReceive(address, regData, 2);
 		return (uint16_t)((data[0] << 8) | data[1]);
 	}
 
 	uint16_t SMBusDevice::readWord()
 	{
-		uint8_t data[2];
 		i2c.speed(rateKhz);
 		// S Addr Wr [A] Comm [A] S Addr Rd [A] [DataLow] A [DataHigh] NA P
-		i2c.sendReceive(address, NULL, 0, data, 2);
+		auto data = i2c.sendReceive(address, std::vector<uint8_t>(), 2);
 		return (uint16_t)((data[1] << 8) | data[0]);
 	}
 
 	uint16_t SMBusDevice::readWordBE()
 	{
-		uint8_t data[2];
 		i2c.speed(rateKhz);
 		// S Addr Wr [A] Comm [A] S Addr Rd [A] [DataHigh] A [DataLow] NA P
-		i2c.sendReceive(address, NULL, 0, data, 2);
+		auto data = i2c.sendReceive(address, std::vector<uint8_t>(), 2);
 		return (uint16_t)((data[0] << 8) | data[1]);
 	}
 
 	void SMBusDevice::writeByteData(uint8_t reg, uint8_t data)
 	{
-		uint8_t dataToSend[2];
-		dataToSend[0] = reg;
-		dataToSend[1] = data;
+		std::vector<uint8_t> dataToSend = { reg, data };
 		i2c.speed(rateKhz);
 		// S Addr Wr [A] Comm [A] Data [A] P
-		i2c.sendReceive(address, dataToSend, 2, NULL, 0);
+		i2c.sendReceive(address, dataToSend);
 	}
 
 	void SMBusDevice::writeWordData(uint8_t reg, uint16_t data)
 	{
-		uint8_t dataToSend[3];
-		dataToSend[0] = reg;
-		dataToSend[1] = (data & 0xFF);
-		dataToSend[2] = (data >> 8);
+		std::vector<uint8_t> dataToSend = { reg, (uint8_t)(data & 0xFF) , (uint8_t)(data >> 8) };
 		i2c.speed(rateKhz);
 		// S Addr Wr [A] Comm [A] DataLow [A] DataHigh [A] P
-		i2c.sendReceive(address, dataToSend, 3, NULL, 0);
+		i2c.sendReceive(address, dataToSend);
 	}
 
 	void SMBusDevice::writeWordDataBE(uint8_t reg, uint16_t data)
 	{
-		uint8_t dataToSend[3];
-		dataToSend[0] = reg;
-		dataToSend[1] = (data >> 8);
-		dataToSend[2] = (data & 0xFF);
+		std::vector<uint8_t> dataToSend = { reg, (uint8_t)(data >> 8), (uint8_t)(data & 0xFF) };
 		i2c.speed(rateKhz);
 		// S Addr Wr [A] Comm [A] DataHigh [A] DataLow [A] P
-		i2c.sendReceive(address, dataToSend, 3, NULL, 0);
+		i2c.sendReceive(address, dataToSend, 0);
 	}
 
-	void SMBusDevice::readBufferData(uint8_t reg, uint8_t* inBuffer, size_t count)
+	std::vector<uint8_t> SMBusDevice::readBufferData(uint8_t reg, size_t count)
 	{
+		std::vector<uint8_t> regData = { reg };
 		i2c.speed(rateKhz);
-		i2c.sendReceive(address, &reg, 1, inBuffer, count);
+		return i2c.sendReceive(address, regData, count);
 	}
 
-	void SMBusDevice::writeBufferData(uint8_t reg, uint8_t* outBuffer, size_t count)
+	void SMBusDevice::writeBufferData(uint8_t reg, std::vector<uint8_t> data)
 	{
+		data.insert(data.begin(), reg);
 		i2c.speed(rateKhz);
-		uint8_t* data = new uint8_t[count + 1];
-		data[0] = reg;
-		copy(outBuffer, outBuffer + count, data + 1);
-		i2c.sendReceive(address, data, count + 1, NULL, 0);
-		delete[] data;
+		i2c.sendReceive(address, data, 0);
 	}
 }
