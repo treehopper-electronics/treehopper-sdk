c5d9069ef90bbefca3d8b051e71d18bb5a2ad4dd
Jay Carlson <jay.d.carlson@gmail.com>
2018-02-25 22:01:49 -0600

    Add MPU6050

---
+++ C++/API/Treehopper.Libraries/inc/Sensors/Inertial/Mpu6050.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Treehopper.Libraries.h"
+#include "Mpu6050Registers.h"
+#include "Accelerometer.h"
+#include "Gyroscope.h"
+#include "SMBusDevice.h"
+#include "Sensors/Temperature/TemperatureSensor.h"
+
+using namespace Treehopper::Libraries::Sensors::Temperature;
+
+namespace Treehopper {
+    namespace Libraries {
+        namespace Sensors {
+            namespace Inertial {
+                class LIBRARIES_API Mpu6050
+                        : virtual public Accelerometer,
+                          virtual public Gyroscope,
+                          virtual public TemperatureSensor {
+                public:
+                    Mpu6050(I2c &i2c, bool addressPin = false, int rate = 100);
+
+                    void update();
+
+                protected:
+                    Mpu6050Registers _registers;
+                private:
+                    SMBusDevice dev;
+                };
+            }
+        }
+    }
+}
