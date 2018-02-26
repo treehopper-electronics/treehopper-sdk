d906e45f5c9cf5bac7a973d8c0c546ea78dbbdea
Jay Carlson <jcarlson@unl.edu>
2018-02-25 19:20:00 -0600

    Export ADXL345

--- C++/API/Treehopper.Libraries/inc/Sensors/Inertial/Adxl345.h
+++ C++/API/Treehopper.Libraries/inc/Sensors/Inertial/Adxl345.h
@@ -6,7 +6,7 @@
 using namespace Treehopper::Libraries;
 
 namespace Treehopper { namespace Libraries { namespace Sensors { namespace Inertial {
-	class Adxl345 : public Accelerometer
+	class LIBRARIES_API Adxl345 : virtual public Accelerometer
 	{
 	public:
 		Adxl345(I2c &i2c, bool altAddress = false, int rate = 100);
