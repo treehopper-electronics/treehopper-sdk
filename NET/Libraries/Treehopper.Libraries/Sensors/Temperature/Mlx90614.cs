4057aa2a568fbde3fbad9b63c24d0331703d6bd0
Jay Carlson <jcarlson@unl.edu>
2018-02-25 23:38:28 -0600

    split MLX9061x classes

--- NET/Libraries/Treehopper.Libraries/Sensors/Temperature/Mlx90614.cs
+++ NET/Libraries/Treehopper.Libraries/Sensors/Temperature/Mlx90614.cs
@@ -69,21 +69,4 @@ namespace Treehopper.Libraries.Sensors.Temperature
             }
         }
     }
-
-    /// <summary>
-    ///     MLX90615 non-contact IR thermopile temperature sensor
-    /// </summary>
-    public class Mlx90615 : Mlx90614
-    {
-        /// <summary>
-        ///     Construct a new MLX90615 attached to the given i2c port
-        /// </summary>
-        /// <param name="module"></param>
-        public Mlx90615(I2C module) : base(module)
-        {
-            dev = new SMBusDevice(0x5B, module);
-            Object = new TempRegister(dev, 0x27);
-            Ambient = new TempRegister(dev, 0x26);
-        }
-    }
 }
