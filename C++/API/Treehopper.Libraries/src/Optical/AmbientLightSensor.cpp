b9b4eb824c8bfef441eba780ae6a498ba55bb0a6
Jay Carlson <jay.d.carlson@gmail.com>
2018-02-25 23:36:15 -0600

    Add BH1750 ambient light sensor

---
+++ C++/API/Treehopper.Libraries/src/Optical/AmbientLightSensor.cpp
@@ -0,0 +1,15 @@
+#include <Sensors/Optical/AmbientLightSensor.h>
+
+namespace Treehopper {
+    namespace Libraries {
+        namespace Sensors {
+            namespace Optical {
+                double AmbientLightSensor::lux() {
+                    if (autoUpdateWhenPropertyRead) update();
+
+                    return _lux;
+                }
+            }
+        }
+    }
+}
