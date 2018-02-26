31f096144f4e0403eb3e39ea395a490194411324
Jay Carlson <jay.d.carlson@gmail.com>
2018-02-25 18:54:54 -0600

    Add ADXL345

--- C++/API/Treehopper.Libraries/inc/Sensors/Inertial/Adxl345.h
+++ C++/API/Treehopper.Libraries/inc/Sensors/Inertial/Adxl345.h
@@ -1,28 +1,18 @@
 #pragma once
 #include "Treehopper.Libraries.h"
 #include "Adxl345Registers.h"
+#include "Accelerometer.h"
 
 using namespace Treehopper::Libraries;
 
-namespace Treehopper {
-	namespace Libraries {
-		namespace Sensors {
-			namespace Inertial {
-				class Adxl345 : public Accelerometer
-				{
-				public:
-					Adxl345(I2c& i2c, bool altAddress = false, int rate = 100) : _dev(!altAddress ? 0x53 : 0x1D, i2c, rate), registers(_dev)
-					{
-						registers.powerCtl.sleep = 0;
-						registers.powerCtl.measure = 1;
-						registers.dataFormat.range = 0x03;
-						registers.flush();
-					}
-				private:
-					Adxl345Registers registers;
-					SMBusDevice _dev;
-				};
-			}
-		}
-	}
-}
+namespace Treehopper { namespace Libraries { namespace Sensors { namespace Inertial {
+	class Adxl345 : public Accelerometer
+	{
+	public:
+		Adxl345(I2c &i2c, bool altAddress = false, int rate = 100);
+        void update();
+	private:
+		Adxl345Registers registers;
+		SMBusDevice _dev;
+	};
+} } } }
