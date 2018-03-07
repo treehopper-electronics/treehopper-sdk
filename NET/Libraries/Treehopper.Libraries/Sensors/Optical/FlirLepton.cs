be1b015dd15e907f266ffff8436ac8dfb151f0cc
Jay Carlson <jcarlson@unl.edu>
2018-03-06 22:46:38 -0600

    Don't hang if board gets disconnected.

--- NET/Libraries/Treehopper.Libraries/Sensors/Optical/FlirLepton.cs
+++ NET/Libraries/Treehopper.Libraries/Sensors/Optical/FlirLepton.cs
@@ -34,12 +34,17 @@ namespace Treehopper.Libraries.Sensors.Optical
         /// <summary>
         ///     Get the raw frame from the sensor
         /// </summary>
+        /// <param name="minimumHeight">Only return frames with the minimum-specified height.</param>
         /// <returns>An awaitable 2D-array of values</returns>
-        public async Task<ushort[,]> GetRawFrameAsync()
+        public async Task<ushort[,]> GetRawFrameAsync(bool timeoutFrame = false, int minimumHeight = 20)
         {
-            //cs.DigitalValue = false;
-            //await Task.Delay(185).ConfigureAwait(false);
-            //cs.DigitalValue = true;
+            if(timeoutFrame)
+            {
+                cs.DigitalValue = false;
+                await Task.Delay(185).ConfigureAwait(false);
+                cs.DigitalValue = true;
+            }
+            
             var frame = new ushort[height, width];
             var frameAcquired = false;
             var syncAcquired = false;
@@ -58,10 +63,13 @@ namespace Treehopper.Libraries.Sensors.Optical
                 }
                 for (var i = 0; i < height; i++)
                 {
+                    if (packet[0] == 0 && packet[1] == 0 && packet[2] == 0 && packet[3] == 0)
+                        frameAcquired = true;
                     if ((packet[0] & 0x000f) == 0x000f)
                     {
                         // lost sync
-                        frameAcquired = true;
+                        if(i >= minimumHeight)
+                            frameAcquired = true;
                         break;
                     }
 
@@ -78,32 +86,6 @@ namespace Treehopper.Libraries.Sensors.Optical
 
             return frame;
         }
-
-//#if WINDOWS_UWP
-//        public async Task<WriteableBitmap> GetFrame()
-//        {
-//            WriteableBitmap frame  = new WriteableBitmap(width, height);
-//            var correctedFrame = await GetCorrectedRasterFrameArray();
-//            byte[] correctedArgbFrame = new byte[width * height * 4];
-//            for (int i=0;i<width*height;i++)
-//            {
-//                correctedArgbFrame[i * 4 + 0] = correctedFrame[i];
-//                correctedArgbFrame[i * 4 + 1] = correctedFrame[i];
-//                correctedArgbFrame[i * 4 + 2] = correctedFrame[i];
-//                correctedArgbFrame[i * 4 + 3] = 0xff;
-//            }
-//            await frame.PixelBuffer.AsStream().WriteAsync(correctedArgbFrame, 0, correctedArgbFrame.Length);
-//            return frame;
-//        }
-//#else
-//        public async Task<WriteableBitmap> GetFrame()
-//        {
-//            WriteableBitmap frame = new WriteableBitmap(80, 60, 300, 300, PixelFormats.Gray8, null);
-//            var correctedFrame = await GetCorrectedRasterFrameArray();
-//            frame.WritePixels(new Int32Rect(0, 0, 80, 60), correctedFrame, 1, 0);
-//            return frame;
-//        }
-//#endif
 
         /// <summary>
         ///     Get the corrected raster frame
@@ -145,59 +127,7 @@ namespace Treehopper.Libraries.Sensors.Optical
             ushort[] packet = new ushort[82];
             
             Buffer.BlockCopy(data, 0, packet, 0, 164);
-            //byte[] data = await spi.SendReceive(new byte[164]);
-            //IntPtr buffer = Marshal.AllocHGlobal(rawsize);
-            //Marshal.Copy(data, 0, buffer, rawsize);
-            //return Marshal.PtrToStructure<VoSPI>(buffer);
             return packet;
-        }
-
-        [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 164)]
-        private struct VoSPI
-        {
-            public readonly ushort Id;
-
-            public readonly ushort Crc;
-
-            //[MarshalAs(UnmanagedType.ByValArray, SizeConst = 80)]
-            public ushort[] Payload;
-        }
-    }
-
-    internal static class Crc16
-    {
-        private const ushort polynomial = 0xA001;
-        private static readonly ushort[] table = new ushort[256];
-
-        static Crc16()
-        {
-            ushort value;
-            ushort temp;
-            for (ushort i = 0; i < table.Length; ++i)
-            {
-                value = 0;
-                temp = i;
-                for (byte j = 0; j < 8; ++j)
-                {
-                    if (((value ^ temp) & 0x0001) != 0)
-                        value = (ushort) ((value >> 1) ^ polynomial);
-                    else
-                        value >>= 1;
-                    temp >>= 1;
-                }
-                table[i] = value;
-            }
-        }
-
-        public static ushort ComputeChecksum(byte[] bytes)
-        {
-            ushort crc = 0;
-            for (var i = 0; i < bytes.Length; ++i)
-            {
-                var index = (byte) (crc ^ bytes[i]);
-                crc = (ushort) ((crc >> 8) ^ table[index]);
-            }
-            return crc;
         }
     }
 }
