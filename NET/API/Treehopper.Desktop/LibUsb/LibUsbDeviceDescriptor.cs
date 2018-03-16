55bf620f6b2dc15fc1f4d4556faca17bbbfdfd60
Jay Carlson <jay.d.carlson@gmail.com>
2018-03-16 10:42:42 -0500

    Still repairing Resharper's destruction

--- NET/API/Treehopper.Desktop/LibUsb/LibUsbDeviceDescriptor.cs
+++ NET/API/Treehopper.Desktop/LibUsb/LibUsbDeviceDescriptor.cs
@@ -5,43 +5,40 @@ namespace Treehopper.Desktop.LibUsb
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public class LibUsbDeviceDescriptor
     {
-        /** Device release number in binary-coded decimal */
-        public ushort bcdDevice;
-
-        /** USB specification release number in binary-coded decimal. A value of
-     * 0x0200 indicates USB 2.0, 0x0110 indicates USB 1.1, etc. */
-        public ushort bcdUSB;
+        /** Size of this descriptor (in bytes) */
+        public byte bLength;
 
         /** Descriptor type. Will have value
      * \ref libusb_descriptor_type::LIBUSB_DT_DEVICE LIBUSB_DT_DEVICE in this
      * context. */
         public byte bDescriptorType;
 
+        /** USB specification release number in binary-coded decimal. A value of
+     * 0x0200 indicates USB 2.0, 0x0110 indicates USB 1.1, etc. */
+        public ushort bcdUSB;
+
         /** USB-IF class code for the device. See \ref libusb_class_code. */
         public byte bDeviceClass;
-
-        /** USB-IF protocol code for the device, qualified by the bDeviceClass and
-     * bDeviceSubClass values */
-        public byte bDeviceProtocol;
 
         /** USB-IF subclass code for the device, qualified by the bDeviceClass
      * value */
         public byte bDeviceSubClass;
 
-        /** Size of this descriptor (in bytes) */
-        public byte bLength;
+        /** USB-IF protocol code for the device, qualified by the bDeviceClass and
+     * bDeviceSubClass values */
+        public byte bDeviceProtocol;
 
         /** Maximum packet size for endpoint 0 */
         public byte bMaxPacketSize0;
 
-        /** Number of possible configurations */
-        public byte bNumConfigurations;
+        /** USB-IF vendor ID */
+        public ushort idVendor;
 
         /** USB-IF product ID */
         public ushort idProduct;
 
-        /** USB-IF vendor ID */
-        public ushort idVendor;
+        /** Device release number in binary-coded decimal */
+        public ushort bcdDevice;
 
         /** Index of string descriptor describing manufacturer */
         public byte iManufacturer;
@@ -51,5 +48,8 @@ namespace Treehopper.Desktop.LibUsb
 
         /** Index of string descriptor containing device serial number */
         public byte iSerialNumber;
+
+        /** Number of possible configurations */
+        public byte bNumConfigurations;
     }
 }
