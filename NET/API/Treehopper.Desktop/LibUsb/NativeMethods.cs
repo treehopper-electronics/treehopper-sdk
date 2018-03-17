097d06c3133e74cd70d9a31237010eff7f13ab34
Jay Carlson <jcarlson@unl.edu>
2018-03-17 15:19:29 -0700

    switch to more-common libusb-1.0.so.0 ref

--- NET/API/Treehopper.Desktop/LibUsb/NativeMethods.cs
+++ NET/API/Treehopper.Desktop/LibUsb/NativeMethods.cs
@@ -10,7 +10,7 @@ namespace Treehopper.Desktop.LibUsb
     internal static class NativeMethods
     {
         internal const CallingConvention CC = 0;
-        internal const string LIBUSB_DLL = "libusb-1.0.so";
+        internal const string LIBUSB_DLL = "libusb-1.0.so.0";
 
         internal const int HotplugMatchAny = -1;
 
