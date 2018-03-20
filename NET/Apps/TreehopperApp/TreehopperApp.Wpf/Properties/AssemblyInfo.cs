ccb337ab24b969b62fb5d2ed05b286c2a1ac1220
Jay Carlson <jcarlson@unl.edu>
2018-03-20 14:22:17 -0500

    Switch runtime to 4.7

--- NET/Apps/TreehopperApp/TreehopperApp.Wpf/Properties/AssemblyInfo.cs
+++ NET/Apps/TreehopperApp/TreehopperApp.Wpf/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TreehopperApp.WPF")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Treehopper")]
+[assembly: AssemblyDescription("Treehopper connects the physical world to your computer, smartphone, or tablet.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TreehopperApp.WPF")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Treehopper Electronics")]
+[assembly: AssemblyProduct("Treehopper.Wpf")]
+[assembly: AssemblyCopyright("Copyright © 2018 Treehopper Electronics")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.11.0.0")]
+[assembly: AssemblyFileVersion("1.11.0.0")]
