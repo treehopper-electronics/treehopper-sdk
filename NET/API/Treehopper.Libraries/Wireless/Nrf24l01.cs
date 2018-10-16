8fc5bae48e9ec6514dd6b6bd1a4c86c5717e2f94
Jay Carlson <jcarlson@unl.edu>
2018-10-16 17:35:29 -0500

    Tx working

--- NET/API/Treehopper.Libraries/Wireless/Nrf24l01.cs
+++ NET/API/Treehopper.Libraries/Wireless/Nrf24l01.cs
@@ -10,6 +10,39 @@ namespace Treehopper.Libraries.Wireless
 {
     public class Nrf24l01
     {
+        public class Pipe
+        {
+            public Pipe(int index, Nrf24l01 rf)
+            {
+                this.index = index;
+                this.rf = rf;
+            }
+
+            private bool dynamicPayloadEnabled;
+            private int index;
+            private Nrf24l01 rf;
+
+            public bool DynamicPayloadEnabled
+            {
+                get { return dynamicPayloadEnabled; }
+                set {
+                    dynamicPayloadEnabled = value;
+                }
+            }
+
+            internal long address;
+
+            public long Address
+            {
+                get { return address; }
+                set {
+                    address = value;
+                    var bytes = BitConverter.GetBytes(address);
+                    Task.Run(() => rf.WriteRegister((byte)((byte)(Registers.RxAddrPipe0)+index), bytes.Take(5).ToArray())).Wait();
+                }
+            }
+        }
+
         public enum CrcMode
         {
             CrcDisabled,
@@ -20,7 +53,7 @@ namespace Treehopper.Libraries.Wireless
         public enum DataRate
         {
             Rate_1mbps,
-            Rate_500kbps,
+            Rate_2mbps,
             Rate_250kbps
         }
 
@@ -31,6 +64,56 @@ namespace Treehopper.Libraries.Wireless
             High,
             Max
         }
+
+        private PowerLevel power;
+
+        public PowerLevel Power
+        {
+            get { return power; }
+            set { power = value; Task.Run(updateSetupRegister).Wait(); }
+        }
+
+
+        private DataRate rate = DataRate.Rate_2mbps;
+
+        public DataRate Rate
+        {
+            get { return rate; }
+            set { rate = value; Task.Run(updateSetupRegister).Wait(); }
+        }
+
+        private Task updateSetupRegister()
+        {
+            var rfPwr = (int)power;
+            var rfDr = 0;
+            switch(rate)
+            {
+                case DataRate.Rate_1mbps:
+                    rfDr = 0b000;
+                    break;
+                case DataRate.Rate_2mbps:
+                    rfDr = 0b001;
+                    break;
+                case DataRate.Rate_250kbps:
+                    rfDr = 0b100;
+                    break;
+            }
+
+            var regVal = rfDr << 3 | rfPwr << 1 | 1;
+            return WriteRegister((byte)Registers.RfSetup, (byte)regVal);
+        }
+
+        private bool rxEnable;
+
+        public bool RxEnable
+        {
+            get { return rxEnable; }
+            set {
+                rxEnable = value;
+
+            }
+        }
+
 
         public class PacketReceivedEventArgs : EventArgs
         {
@@ -52,30 +135,78 @@ namespace Treehopper.Libraries.Wireless
             Task.Run(cePin.MakeDigitalPushPullOutAsync).Wait();
             cePin.DigitalValue = false;
             irqPin.DigitalValueChanged += IrqPin_DigitalValueChanged;
+
+            Task.Run(() => dev.SendReceiveAsync(new byte[] { 0x50, 0x73 })).Wait(); // enable feature register
+            Task.Run(() => WriteRegister((byte)Registers.Feature, 0b00000111)).Wait();
+            Task.Run(() => WriteRegister((byte)Registers.DynamicPayloadLength, 0b00111111)).Wait();
+            Task.Run(FlushRx).Wait();
+            Task.Run(FlushTx).Wait();
+
             Task.Run(writeConfig).Wait();
-            Task.Run(() => dev.SendReceiveAsync(new byte[] {0x50, 0x73 })).Wait();
-            Task.Run(() => WriteRegister((byte)Registers.Feature, 0x03)).Wait();
+
+            for (int i = 0; i < 6; i++)
+            {
+                pipes[i] = new Pipe(i, this);
+            }
+            // set default addresses
+            pipes[0].address = 0xE7E7E7E7E7;
+            pipes[1].address = 0xC2C2C2C2C2;
+            pipes[2].address = 0xC2C2C2C2C3;
+            pipes[3].address = 0xC2C2C2C2C4;
+            pipes[4].address = 0xC2C2C2C2C5;
+            pipes[5].address = 0xC2C2C2C2C6;
+
+            Task.Run(() => Task.Delay(5)).Wait();
         }
 
-        private void IrqPin_DigitalValueChanged(object sender, DigitalInValueChangedEventArgs e)
+        private Pipe[] pipes = new Pipe[6];
+
+        public Pipe[] Pipes => pipes;
+
+        public Pipe this[int i]
+        {
+            get
+            {
+                return pipes[i];
+            }
+        }
+
+        private async void IrqPin_DigitalValueChanged(object sender, DigitalInValueChangedEventArgs e)
         {
             Debug.WriteLine("IRQ");
+            var data = new byte[] { (byte)((byte)Registers.Status | 0x20), 0b01110000 };
+            var bytes = await dev.SendReceiveAsync(data);
+            DebugStatus(bytes[0]);
         }
 
-        private int retryCount;
+        private int retryCount = 3;
 
         public int RetryCount
         {
             get { return retryCount; }
-            set { retryCount = value; }
+            set {
+                retryCount = value;
+                Task.Run(updateAutoRetransmission).Wait();
+            }
         }
 
-        private int retryDelay;
+        private int retryDelayUs = 250;
 
-        public int RetryDelay
+        public int RetryDelayUs
         {
-            get { return retryDelay; }
-            set { retryDelay = value; }
+            get { return retryDelayUs; }
+            set {
+                retryDelayUs = value;
+                Task.Run(updateAutoRetransmission).Wait();
+            }
+        }
+
+        private Task updateAutoRetransmission()
+        {
+            var ard = Numbers.Constrain((int)(retryDelayUs / 250.0) - 1, 0, 15);
+            var arc = Numbers.Constrain(retryCount, 0, 15);
+            var regVal = (ard << 4) | arc;
+            return WriteRegister((byte)Registers.SetupAutomaticRetransmission, (byte)regVal);
         }
 
         private CrcMode crc = CrcMode.Crc8Bit;
@@ -87,6 +218,14 @@ namespace Treehopper.Libraries.Wireless
                 crc = value;
                 Task.Run(writeConfig).Wait();
             }
+        }
+
+        private bool enhancedShockburstEnabled = false;
+
+        public bool EnhancedShockburstEnabled
+        {
+            get { return enhancedShockburstEnabled; }
+            set { enhancedShockburstEnabled = value; }
         }
 
         private Task writeConfig()
@@ -118,7 +257,21 @@ namespace Treehopper.Libraries.Wireless
             set { autoAckEnabled = value; }
         }
 
-        private int channel = 76; // this is the default power-up channel
+        private bool ackPayloadEnabled;
+        public bool AckPayloadEnabled
+        {
+            get { return ackPayloadEnabled;}
+            set { ackPayloadEnabled = value;}
+        }
+        
+        private byte[] ackPayload;
+        public byte[] AckPayload
+        {
+            get { return ackPayload;}
+            set { ackPayload = value;}
+        }
+
+        private int channel = 2; // this is the default power-up channel
 
         public int Channel
         {
@@ -151,14 +304,27 @@ namespace Treehopper.Libraries.Wireless
             }
         }
 
-        private long address = 0xE7E7E7E7E7; // default address, per NRF datasheet
+        private long txAddress = 0xE7E7E7E7E7; // default address, per NRF datasheet
 
-        public long Address
+        public long TxAddress
         {
-            get { return address; }
-            set { address = value; }
+            get { return txAddress; }
+            set {
+                txAddress = value;
+                var bytes = BitConverter.GetBytes(TxAddress);
+                Task.Run(() => WriteRegister((byte)Registers.TxAddr, bytes.Take(5).ToArray())).Wait();
+            }
         }
 
+        public Task FlushRx()
+        {
+            return dev.SendReceiveAsync(new byte[] { 0b11100010 });
+        }
+
+        public Task FlushTx()
+        {
+            return dev.SendReceiveAsync(new byte[] { 0b11100001 });
+        }
 
         /// <summary>
         /// Transmit a packet to the specified receiver slot
@@ -168,7 +334,10 @@ namespace Treehopper.Libraries.Wireless
         /// <returns></returns>
         public async Task Transmit(byte[] data, bool requestAck=true)
         {
-
+            var dataToSend = new byte[] { 0xA0 }.Concat(data).ToArray();
+            await dev.SendReceiveAsync(dataToSend);
+            cePin.DigitalValue = true;
+            cePin.DigitalValue = false;
         }
 
         private async Task<byte> ReadRegister(byte register)
@@ -181,6 +350,7 @@ namespace Treehopper.Libraries.Wireless
         private async Task<byte[]> ReadRegister(byte register, int numBytesToRead)
         {
             var bytes = new byte[numBytesToRead + 1];
+            bytes[0] = register;
             for (int i = 0; i < numBytesToRead; i++)
                 bytes[i + 1] = 0xff;
 
@@ -192,12 +362,63 @@ namespace Treehopper.Libraries.Wireless
         {
             var data = new byte[] { (byte)(register | 0x20), value };
             await dev.SendReceiveAsync(data, SpiBurstMode.BurstTx);
+            //await Task.Delay(100);
+            //var result = await ReadRegister(register);
+            //if (result != value)
+            //{
+            //    Debug.WriteLine("ERROR: Register read-back failed");
+            //}
         }
 
-        private async Task WriteRegister(byte register, byte[] data)
+        private Task WriteRegister(byte register, byte[] data)
         {
             var dataToSend = new byte[] { (byte)(register | 0x20) }.Concat(data).ToArray();
-            await dev.SendReceiveAsync(dataToSend, SpiBurstMode.BurstTx);
+            return dev.SendReceiveAsync(dataToSend, SpiBurstMode.BurstTx);
+        }
+
+        public Task<byte> GetStatus()
+        {
+            return ReadRegister((byte)Registers.Status);
+        }
+
+        public void DebugStatus(byte status)
+        {
+            if((status & 0x01) == 0x01)
+            {
+                Debug.WriteLine("TX_FULL");
+            }
+            if ((status >> 4 & 0x01) == 0x01)
+            {
+                Debug.WriteLine("MAX_RT");
+            }
+            if ((status >> 5 & 0x01) == 0x01)
+            {
+                Debug.WriteLine("TX_DS");
+            }
+            if ((status >> 6 & 0x01) == 0x01)
+            {
+                Debug.WriteLine("RX_DR");
+            }
+
+            if(((status >> 1) & 0b111) == 0b111)
+                Debug.WriteLine("No bytes received");
+        }
+
+        public async Task DebugDetails()
+        {
+            Debug.WriteLine("STATUS: ");
+            DebugStatus(await GetStatus());
+
+            Debug.WriteLine("Config: {0}", await ReadRegister((byte)Registers.NrfConfig));
+            Debug.WriteLine("Enhanced Shockburst: {0}", await ReadRegister((byte)Registers.EnableAutoAcknowledgment));
+            Debug.WriteLine("Enabled RX addresses: {0}", await ReadRegister((byte)Registers.EnableRxAddresses));
+            Debug.WriteLine("Address widths: {0}", await ReadRegister((byte)Registers.SetupAddressWidth));
+            Debug.WriteLine("Retransmission: {0}", await ReadRegister((byte)Registers.SetupAutomaticRetransmission));
+            Debug.WriteLine("RF Channel: {0}", await ReadRegister((byte)Registers.RfChannel));
+            Debug.WriteLine("RF Setup: {0}", await ReadRegister((byte)Registers.RfSetup));
+            Debug.WriteLine("TX Address: " + (await ReadRegister((byte)Registers.TxAddr, 5)).ToHexString());
+            Debug.WriteLine("RX0 Address: " + (await ReadRegister((byte)Registers.RxAddrPipe0, 5)).ToHexString());
+            Debug.WriteLine("RX1 Address: " + (await ReadRegister((byte)Registers.RxAddrPipe1, 5)).ToHexString());
         }
     }
 
@@ -227,10 +448,7 @@ namespace Treehopper.Libraries.Wireless
         RxPwP4,
         RxPwP5,
         FifoStatus,
-        AckPld,
-        TxPld,
-        RxPld,
-        DynamicPayloadLength,
-        Feature
+        DynamicPayloadLength = 0x1C,
+        Feature = 0x1D
     }
 }
--- NET/Apps/Sandbox/Program.cs
+++ NET/Apps/Sandbox/Program.cs
@@ -22,21 +22,59 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
-            App();
+            RxApp();
         }
 
-        static async Task App()
+        static async Task RxApp()
+        {
+            var board = await ConnectionService.Instance.GetFirstDeviceAsync();
+            await board.ConnectAsync();
+
+            var rf = new Nrf24l01(board.Spi, board.Pins[3], board.Pins[4], board.Pins[5]);
+            rf.TxAddress = 0xC2C2C2;
+            rf.Pipes[0].Address = 0xC2C2C2;
+            rf.AddressWidth = 3;
+            rf.RetryCount = 5;
+            rf.RetryDelayUs = 1500;
+            rf.Channel = 9;
+            rf.Rate = Nrf24l01.DataRate.Rate_250kbps;
+
+
+            while (true)
+            {
+                for (int i = 0; i < 255; i++)
+                {
+                    await rf.Transmit(new byte[] { (byte)i, 0x00, 0x23, 0x37, 0x37, 0x13 }, false);
+                    await Task.Delay(100);
+                }
+
+            }
+
+        }
+
+        static async Task TxApp()
         {
 			var board = await ConnectionService.Instance.GetFirstDeviceAsync();
 			await board.ConnectAsync();
 
             var rf = new Nrf24l01(board.Spi, board.Pins[3], board.Pins[4], board.Pins[5]);
-            var address = BitConverter.ToInt32(Encoding.ASCII.GetBytes("2Node\0"), 0);
+            rf.TxAddress = 0xC2C2C2;
+            rf.Pipes[0].Address = 0xC2C2C2;
+            rf.AddressWidth = 3;
+            rf.RetryCount = 5;
+            rf.RetryDelayUs = 1500;
+            rf.Channel = 9;
+            rf.Rate = Nrf24l01.DataRate.Rate_250kbps;
+
 
             while(true)
             {
-                await rf.Transmit(address, new byte[] { 0x00, 0x01, 0x02 }, false);
-                await Task.Delay(1000);
+                for(int i=0;i<255;i++)
+                {
+                    await rf.Transmit(new byte[] { (byte)i, 0x00, 0x23, 0x37, 0x37, 0x13 }, false);
+                    await Task.Delay(100);
+                }
+                    
             }
             
         }
