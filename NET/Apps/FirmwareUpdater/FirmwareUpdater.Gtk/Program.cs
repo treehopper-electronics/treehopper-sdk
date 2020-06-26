98b715aa24ca2addb6bf2ee33717ce923e3603fd
Jay <jay.d.carlson@gmail.com>
2020-06-26 15:08:52 -0500

    More work on FirmwareUpdater

---
+++ NET/Apps/FirmwareUpdater/FirmwareUpdater.Gtk/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+using Gtk;
+
+namespace FirmwareUpdater.GTK
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("Game of Life");
+            window.Show();
+
+            Gtk.Application.Run();
+        }
+    }
+}
