53d60a93bb62af770f5b5eb2d22fe3dd23ef6b0c
Charles Lee <clee781@gmail.com>
2026-01-29 17:29:30 -0600

    update Blink demo to .NET10

--- NET/Demos/Console/Blink/Program.cs
+++ NET/Demos/Console/Blink/Program.cs
@@ -1,48 +1,20 @@
-﻿using System;
-using Treehopper;
-using System.Threading.Tasks;
-using Treehopper.Desktop;
-using System.Windows.Threading;
+﻿using Treehopper;
 
-namespace Blink
+Console.Write("Waiting for board...");
+// Get a reference to the first TreehopperUsb board connected. This will await indefinitely until a board is connected.
+var board = await ConnectionService.Instance.GetFirstDeviceAsync();
+Console.WriteLine("Found board: " + board);
+Console.WriteLine("Version: " + board.VersionString);
+
+// You must explicitly connect to a board before communicating with it
+await board.ConnectAsync();
+
+Console.WriteLine("Start blinking. Press any key to stop.");
+while (board.IsConnected && !Console.KeyAvailable)
 {
-    /// <summary>
-    /// This demo blinks the built-in LED using async programming.
-    /// </summary>
-    class Program
-    {
-        static TreehopperUsb Board;
-
-        //[MTAThread]
-        static void Main(string[] args)
-        {
-            // We can't do async calls from the Console's Main() function, so we run all our code in a separate async method.
-            Task.Run(RunBlink).Wait();
-        }
-
-        static async Task RunBlink()
-        {
-            while (true)
-            {
-                Console.Write("Waiting for board...");
-                // Get a reference to the first TreehopperUsb board connected. This will await indefinitely until a board is connected.
-                Board = await ConnectionService.Instance.GetFirstDeviceAsync();
-				Console.WriteLine("Found board: " + Board);
-				Console.WriteLine("Version: " + Board.VersionString);
-
-				// You must explicitly connect to a board before communicating with it
-				await Board.ConnectAsync();
-
-				Console.WriteLine("Start blinking. Press any key to stop.");
-				while (Board.IsConnected && !Console.KeyAvailable)
-				{
-					// toggle the LED.
-					Board.Led = !Board.Led;
-					await Task.Delay(100);
-				}
-
-                Board.Disconnect();
-            }
-        }
-    }
+    // toggle the LED.
+    board.Led = !board.Led;
+    await Task.Delay(100);
 }
+
+board.Disconnect();
