e331c887a932b0551f53a08cb5260dff78d4e1db
Charles Lee <clee781@gmail.com>
2026-01-29 23:05:16 -0600

    update input projects

--- NET/Demos/Libraries/Input/RotaryEncoder/Program.cs
+++ NET/Demos/Libraries/Input/RotaryEncoder/Program.cs
@@ -1,31 +1,15 @@
-﻿using System;
-using System.Threading.Tasks;
-using Treehopper;
+﻿using Treehopper;
 using Treehopper.Libraries.Input;
 
-namespace RotaryEncoderDemo
+var board = await ConnectionService.Instance.GetFirstDeviceAsync();
+await board.ConnectAsync();
+
+var encoder = new RotaryEncoder(board.Pins[0], board.Pins[1], 4);
+encoder.MinValue = 0;
+encoder.MaxValue = 19;
+encoder.PositionChanged += Encoder_PositionChanged;
+
+static void Encoder_PositionChanged(object sender, RotaryEncoder.PositionChangedEventArgs e)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            App();
-        }
-
-        static async Task App()
-        {
-            var board = await ConnectionService.Instance.GetFirstDeviceAsync();
-            await board.ConnectAsync();
-
-            var encoder = new RotaryEncoder(board.Pins[0], board.Pins[1], 4);
-            encoder.MinValue = 0;
-            encoder.MaxValue = 19;
-            encoder.PositionChanged += Encoder_PositionChanged;
-        }
-
-        private static void Encoder_PositionChanged(object sender, RotaryEncoder.PositionChangedEventArgs e)
-        {
-            Console.WriteLine($"New position: {e.NewPosition}");
-        }
-    }
+    Console.WriteLine($"New position: {e.NewPosition}");
 }
--- NET/Demos/Libraries/Input/WiiNunchukDemo/Program.cs
+++ NET/Demos/Libraries/Input/WiiNunchukDemo/Program.cs
@@ -1,61 +1,40 @@
-﻿using System;
-using System.Threading.Tasks;
-using Treehopper;
-using Treehopper.Desktop;
+﻿using Treehopper;
 using Treehopper.Libraries.Input;
 using Treehopper.Libraries.Sensors;
 
-namespace WiiNunchukDemo
+var board = await ConnectionService.Instance.GetFirstDeviceAsync();
+await board.ConnectAsync();
+
+var nunchuk = new WiiNunchuk(board.I2c);
+
+// Let a Poller take care of the updating
+using (var poller = new Poller<WiiNunchuk>(nunchuk))
 {
-    class Program
+    // Hook onto some fun events
+    nunchuk.JoystickChanged += Nunchuk_JoystickChanged;
+    nunchuk.C.OnPressed += C_OnPressed;
+    nunchuk.Z.OnPressed += Z_OnPressed;
+
+    Console.WriteLine("Starting demo. Press any key to stop...");
+
+    while (board.IsConnected && !Console.KeyAvailable)
     {
-        static TreehopperUsb board;
-        static void Main(string[] args)
-        {
-            App().Wait();
-            board.Disconnect();
-            Console.WriteLine("Board disconnected");
-        }
-
-        static async Task App()
-        {
-            board = await ConnectionService.Instance.GetFirstDeviceAsync();
-            await board.ConnectAsync();
-
-            var nunchuk = new WiiNunchuk(board.I2c);
-
-            // Let a Poller take care of the updating
-            using (var poller = new Poller<WiiNunchuk>(nunchuk))
-            {
-                // Hook onto some fun events
-                nunchuk.JoystickChanged += Nunchuk_JoystickChanged;
-                nunchuk.C.OnPressed += C_OnPressed;
-                nunchuk.Z.OnPressed += Z_OnPressed;
-
-                Console.WriteLine("Starting demo. Press any key to stop...");
-
-                while (board.IsConnected && !Console.KeyAvailable)
-                {
-                    await Task.Delay(100);
-                }
-            }
-        }
-
-        private static void C_OnPressed(object sender, Button.ButtonPressedEventArgs e)
-        {
-            // the implicity-called ToString() method on the button object (sender, in this context) will print "pressed" or "not pressed" -- handy for console/debug logging!
-            Console.WriteLine("Button C: " + sender);
-        }
-
-        private static void Z_OnPressed(object sender, Button.ButtonPressedEventArgs e)
-        {
-            Console.WriteLine("Button Z: " + sender);
-        }
-
-
-        private static void Nunchuk_JoystickChanged(object sender, WiiNunchuk.JoystickEventArgs e)
-        {
-            Console.WriteLine("Joystick: {0:0.00}, {1:0.00}", e.NewValue.X, e.NewValue.Y);
-        }
+        await Task.Delay(100);
     }
+}
+static void C_OnPressed(object sender, Button.ButtonPressedEventArgs e)
+{
+    // the implicity-called ToString() method on the button object (sender, in this context) will print "pressed" or "not pressed" -- handy for console/debug logging!
+    Console.WriteLine("Button C: " + sender);
+}
+
+static void Z_OnPressed(object sender, Button.ButtonPressedEventArgs e)
+{
+    Console.WriteLine("Button Z: " + sender);
+}
+
+
+static void Nunchuk_JoystickChanged(object sender, WiiNunchuk.JoystickEventArgs e)
+{
+    Console.WriteLine("Joystick: {0:0.00}, {1:0.00}", e.NewValue.X, e.NewValue.Y);
 }
