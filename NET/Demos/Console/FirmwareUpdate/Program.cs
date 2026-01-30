f9be77e86379fda23a8d625b2b602ebc6b1867af
Charles Lee <clee781@gmail.com>
2026-01-29 18:05:37 -0600

    migrate FirmwareUpdate console demo to .NET10

--- NET/Demos/Console/FirmwareUpdate/Program.cs
+++ NET/Demos/Console/FirmwareUpdate/Program.cs
@@ -1,153 +1,137 @@
-﻿using System;
-using System.Threading.Tasks;
-using Treehopper;
+﻿using Treehopper;
 using Treehopper.Firmware;
 
-namespace FirmwareUpdate
+while (true)
 {
-    class Program
+    Console.WriteLine("Press P to program, and T to test");
+    var response = Console.ReadLine();
+
+    if (response.ToLower() == "t")
     {
-        static void Main(string[] args)
+        var board = await ConnectionService.Instance.GetFirstDeviceAsync();
+        await SelfTestAsync(board).ConfigureAwait(false);
+    }
+    else
+    {
+        while (FirmwareConnectionService.Instance.Boards.Count == 0)
         {
-            Console.WriteLine("Treehopper Firmware Updater");
-            Run().Wait();
+            Console.WriteLine("Couldn't find a Treehopper board in bootloader mode. Searching for Treehoppers to reboot...");
+            var board = await ConnectionService.Instance.GetFirstDeviceAsync();
+            if (board != null)
+            {
+                Console.WriteLine("Found board. Connecting to reboot into bootloader mode");
+                if (!await board.ConnectAsync())
+                    Console.WriteLine("Couldn't connect to board. Retrying in 5 seconds...");
+                else
+                    board.RebootIntoBootloader();
+            }
+            Console.WriteLine("Waiting 5 seconds to reconnect...");
+            await Task.Delay(5000);
         }
 
-        static async Task Run()
+        var dfu = FirmwareConnectionService.Instance.Boards[0];
+        dfu.ProgressChanged += Dfu_ProgressChanged;
+
+        Console.WriteLine("Ready to load firmware. Would you like to load with the built-in default? (Y/n)");
+        response = Console.ReadLine();
+        if (response.ToLower() == "y" || response.Length == 0)
         {
-            while(true)
-            {
-                Console.WriteLine("Press P to program, and T to test");
-                var response = Console.ReadLine();
-
-                if (response.ToLower() == "t")
-                {
-                    var board = await ConnectionService.Instance.GetFirstDeviceAsync();
-                    await SelfTestAsync(board).ConfigureAwait(false);
-                } else
-                {
-                    while (FirmwareConnectionService.Instance.Boards.Count == 0)
-                    {
-                        Console.WriteLine("Couldn't find a Treehopper board in bootloader mode. Searching for Treehoppers to reboot...");
-                        var board = await ConnectionService.Instance.GetFirstDeviceAsync();
-                        if (board != null)
-                        {
-                            Console.WriteLine("Found board. Connecting to reboot into bootloader mode");
-                            if (!await board.ConnectAsync())
-                                Console.WriteLine("Couldn't connect to board. Retrying in 5 seconds...");
-                            else
-                                board.RebootIntoBootloader();
-                        }
-                        Console.WriteLine("Waiting 5 seconds to reconnect...");
-                        await Task.Delay(5000);
-                    }
-
-                    var dfu = FirmwareConnectionService.Instance.Boards[0];
-                    dfu.ProgressChanged += Dfu_ProgressChanged;
-
-                    Console.WriteLine("Ready to load firmware. Would you like to load with the built-in default? (Y/n)");
-                    response = Console.ReadLine();
-                    if (response.ToLower() == "y" || response.Length == 0)
-                    {
-                        Console.WriteLine("Loading default firmware from the Treehopper assembly");
-                        await dfu.LoadAsync();
-                    }
-                    else
-                    {
-                        Console.Write("Specify the full file name to use: ");
-                        response = Console.ReadLine();
-                        await dfu.Load(response);
-                    }
-                    Console.WriteLine();
-                    Console.WriteLine("Firmware loaded! Press any key to start again, or close this window to exit.");
-                    Console.ReadKey();
-                }
-
-                
-            }
+            Console.WriteLine("Loading default firmware from the Treehopper assembly");
+            await dfu.LoadAsync();
         }
-
-        private static async Task<bool> SelfTestAsync(TreehopperUsb board)
+        else
         {
-            Console.WriteLine($"Beginning self-test of {board}");
-            bool retVal = true;
-            await board.ConnectAsync().ConfigureAwait(false);
-            // make each pin an input
-            foreach (var pin in board.Pins)
-            {
-                await pin.MakeAnalogInAsync().ConfigureAwait(false);
-                await board.AwaitPinUpdateAsync().ConfigureAwait(false);
-                await Task.Delay(10).ConfigureAwait(false);
-
-                if (pin.AnalogValue < 0.1)
-                {
-                    retVal = false;
-                    Console.WriteLine($"{pin.Name} shorted to ground.");
-                }
-            }
-
-
-            for (int i = 0; i < board.Pins.Count - 1; i++)
-            {
-                board.Pins[i].Mode = PinMode.PushPullOutput;
-                await board.Pins[i].WriteDigitalValueAsync(false).ConfigureAwait(false);
-                await Task.Delay(10).ConfigureAwait(false);
-                await board.AwaitPinUpdateAsync().ConfigureAwait(false);
-                if (board.Pins[i + 1].AnalogValue < 0.1)
-                {
-                    Console.WriteLine($"Short detected on pins {i} and {i + 1}.");
-                    retVal = false;
-                }
-                await board.Pins[i].WriteDigitalValueAsync(true).ConfigureAwait(false);
-            }
-            if (retVal == false)
-            {
-                Console.WriteLine("Errors found during self-test!");
-            }
-            else
-            {
-                Console.WriteLine("Self-test passed!");
-            }
-
-            return retVal;
+            Console.Write("Specify the full file name to use: ");
+            response = Console.ReadLine();
+            await dfu.Load(response);
         }
+        Console.WriteLine();
+        Console.WriteLine("Firmware loaded! Press any key to start again, or close this window to exit.");
+        Console.ReadKey();
+    }
 
-        private static void Dfu_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+
+}
+
+static async Task<bool> SelfTestAsync(TreehopperUsb board)
+{
+    Console.WriteLine($"Beginning self-test of {board}");
+    bool retVal = true;
+    await board.ConnectAsync().ConfigureAwait(false);
+    // make each pin an input
+    foreach (var pin in board.Pins)
+    {
+        await pin.MakeAnalogInAsync().ConfigureAwait(false);
+        await board.AwaitPinUpdateAsync().ConfigureAwait(false);
+        await Task.Delay(10).ConfigureAwait(false);
+
+        if (pin.AnalogValue < 0.1)
         {
-            drawTextProgressBar(e.ProgressPercentage, 100);
-        }
-
-        private static void drawTextProgressBar(int progress, int total)
-        {
-            //draw empty progress bar
-            Console.CursorLeft = 0;
-            Console.Write("["); //start
-            Console.CursorLeft = 32;
-            Console.Write("]"); //end
-            Console.CursorLeft = 1;
-            float onechunk = 30.0f / total;
-
-            //draw filled part
-            int position = 1;
-            for (int i = 0; i < onechunk * progress; i++)
-            {
-                Console.BackgroundColor = ConsoleColor.Gray;
-                Console.CursorLeft = position++;
-                Console.Write(" ");
-            }
-
-            //draw unfilled part
-            for (int i = position; i <= 31; i++)
-            {
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.CursorLeft = position++;
-                Console.Write(" ");
-            }
-
-            //draw totals
-            Console.CursorLeft = 35;
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.Write(progress.ToString() + " of " + total.ToString() + "    "); //blanks at the end remove any excess
+            retVal = false;
+            Console.WriteLine($"{pin.Name} shorted to ground.");
         }
     }
+
+
+    for (int i = 0; i < board.Pins.Count - 1; i++)
+    {
+        board.Pins[i].Mode = PinMode.PushPullOutput;
+        await board.Pins[i].WriteDigitalValueAsync(false).ConfigureAwait(false);
+        await Task.Delay(10).ConfigureAwait(false);
+        await board.AwaitPinUpdateAsync().ConfigureAwait(false);
+        if (board.Pins[i + 1].AnalogValue < 0.1)
+        {
+            Console.WriteLine($"Short detected on pins {i} and {i + 1}.");
+            retVal = false;
+        }
+        await board.Pins[i].WriteDigitalValueAsync(true).ConfigureAwait(false);
+    }
+    if (retVal == false)
+    {
+        Console.WriteLine("Errors found during self-test!");
+    }
+    else
+    {
+        Console.WriteLine("Self-test passed!");
+    }
+
+    return retVal;
+}
+
+static void Dfu_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+{
+    drawTextProgressBar(e.ProgressPercentage, 100);
+}
+
+static void drawTextProgressBar(int progress, int total)
+{
+    //draw empty progress bar
+    Console.CursorLeft = 0;
+    Console.Write("["); //start
+    Console.CursorLeft = 32;
+    Console.Write("]"); //end
+    Console.CursorLeft = 1;
+    float onechunk = 30.0f / total;
+
+    //draw filled part
+    int position = 1;
+    for (int i = 0; i < onechunk * progress; i++)
+    {
+        Console.BackgroundColor = ConsoleColor.Gray;
+        Console.CursorLeft = position++;
+        Console.Write(" ");
+    }
+
+    //draw unfilled part
+    for (int i = position; i <= 31; i++)
+    {
+        Console.BackgroundColor = ConsoleColor.Green;
+        Console.CursorLeft = position++;
+        Console.Write(" ");
+    }
+
+    //draw totals
+    Console.CursorLeft = 35;
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.Write(progress.ToString() + " of " + total.ToString() + "    "); //blanks at the end remove any excess
 }
